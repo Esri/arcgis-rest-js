4d729f24a047154eea7932c5024d1cdf9d8ba91c
Patrick Arlt <patrick.arlt@gmail.com>
2017-08-09 08:41:34 -0700

    add diff reporter for karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["progress", "karma-typescript"],
+    reporters: ["jasmine-diff", "progress", "karma-typescript"],
 
     // web server port
     port: 9876,
