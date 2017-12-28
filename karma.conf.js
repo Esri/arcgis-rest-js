a082a793addb67566a47d8dc36de3843165d9436
john gravois <jgravois@esri.com>
2017-12-28 10:29:03 -0800

    have travis target headless firefox too

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
       type: 'lcov',
       dir: 'coverage/'
     },
-    
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
@@ -57,7 +57,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["jasmine-diff", "dots", "karma-typescript", "coverage", "coveralls"],
+    reporters: ["jasmine-diff", "dots", "karma-typescript", "coverage"],
 
     // web server port
     port: 9876,
