cf1108e19f3516a19f1e0278dfc8898bcf86deb6
Tom Wayson <tom@tomwayson.com>
2018-02-20 00:12:41 -0800

    add queryFeatures() to send query requests to feature services

--- packages/arcgis-rest-feature-service/test/features.test.ts
+++ packages/arcgis-rest-feature-service/test/features.test.ts
@@ -1,8 +1,8 @@
-import { getFeature } from "../src/index";
+import { getFeature, queryFeatures } from "../src/index";
 
 import * as fetchMock from "fetch-mock";
 
-import { featureResponse } from "./mocks/feature";
+import { featureResponse, queryResponse } from "./mocks/feature";
 
 describe("feature", () => {
   afterEach(fetchMock.restore);
@@ -20,6 +20,22 @@ describe("feature", () => {
       expect(url).toEqual(`${params.url}/42?f=json`);
       expect(options.method).toBe("GET");
       expect(response.attributes.FID).toEqual(42);
+      done();
+    });
+  });
+
+  it("should supply default query parameters", done => {
+    const params = {
+      url:
+        "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
+    };
+    fetchMock.once("*", queryResponse);
+    queryFeatures(params).then(response => {
+      expect(fetchMock.called()).toBeTruthy();
+      const [url, options]: [string, RequestInit] = fetchMock.lastCall("*");
+      expect(url).toEqual(`${params.url}/query?f=json&where=1%3D1&outFields=*`);
+      expect(options.method).toBe("GET");
+      // expect(response.attributes.FID).toEqual(42);
       done();
     });
   });
