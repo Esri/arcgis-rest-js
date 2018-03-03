302fa85227a30cb8369528fb86d68962ca1bf911
Tom Wayson <tom@tomwayson.com>
2018-03-03 11:08:22 -0800

    .umd.js = unminified; .umd.min.js = minified

--- umd-base-profile.js
+++ umd-base-profile.js
@@ -59,8 +59,8 @@ const globals = packageNames.reduce((globals, p) => {
 export default {
   input: "./src/index.ts",
   output: {
-    file: `./dist/umd/${name.replace("@esri/", "")}.debug.umd.js`,
-    sourcemap: `./dist/umd/${name.replace("@esri/", "")}.debug.umd.js.map`,
+    file: `./dist/umd/${name.replace("@esri/", "")}.umd.js`,
+    sourcemap: `./dist/umd/${name.replace("@esri/", "")}.umd.js.map`,
     banner: copyright,
     format: "umd",
     name: moduleName,
--- umd-production-profile.js
+++ umd-production-profile.js
@@ -3,8 +3,8 @@ import uglify from "rollup-plugin-uglify";
 import filesize from "rollup-plugin-filesize";
 
 // remove 'debug' from the unminified UMD filename and sourcemap
-config.output.file = config.output.file.replace(".debug.umd.", ".umd.");
-config.output.sourcemap = config.output.sourcemap.replace(".debug.umd.", ".umd.");
+config.output.file = config.output.file.replace(".umd.", ".umd.min.");
+config.output.sourcemap = config.output.sourcemap.replace(".umd.", ".umd.min.");
 
 config.plugins.push(filesize())
 config.plugins.push(uglify({ 
