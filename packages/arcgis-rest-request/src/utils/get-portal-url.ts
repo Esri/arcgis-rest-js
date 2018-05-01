61031012d249fcaa9d86b5c68c9cbe7489b7a3b5
Noah Mulfinger <noah.mulfinger@gmail.com>
2018-05-01 14:24:09 -0700

    fix(getPortalUrl): make getPortalUrl use portal in request options if passed in
    AFFECTS PACKAGES:
    @esri/arcgis-rest-request
    
    ISSUES CLOSED: #180

--- packages/arcgis-rest-request/src/utils/get-portal-url.ts
+++ packages/arcgis-rest-request/src/utils/get-portal-url.ts
@@ -5,17 +5,21 @@ import { IRequestOptions } from "../request";
 /**
  * Helper that returns the portalUrl - either defaulting to www.arcgis.com or using
  * the passed in auth manager's .portal property
- * 
+ *
  * @param requestOptions - Request options that may have authentication manager
  * @returns Portal url to be used in API requests
  */
-export function getPortalUrl(requestOptions?: IRequestOptions): string {
-  // default to arcgis.com
-  let portalUrl = "https://www.arcgis.com/sharing/rest";
-  // but if the auth was passed, use that portal...
-  if (requestOptions && requestOptions.authentication) {
-    portalUrl = requestOptions.authentication.portal;
+export function getPortalUrl(requestOptions: IRequestOptions = {}): string {
+  // use portal in options if specified
+  if (requestOptions.portal) {
+    return requestOptions.portal;
   }
 
-  return portalUrl;
+  // if the auth was passed, use that portal
+  if (requestOptions.authentication) {
+    return requestOptions.authentication.portal;
+  }
+
+  // default to arcgis.com
+  return "https://www.arcgis.com/sharing/rest";
 }
