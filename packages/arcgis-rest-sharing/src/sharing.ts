a212d59abf820f2e719aaaedb85dd3f3708dc793
john gravois <jgravois@esri.com>
2018-05-23 17:10:26 -0700

    feat(caring): new sharing package with method to set access on items
    AFFECTS PACKAGES:
    @esri/arcgis-rest-sharing
    
    ISSUES CLOSED: #43

---
+++ packages/arcgis-rest-sharing/src/sharing.ts
@@ -0,0 +1,107 @@
+/* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
+ * Apache-2.0 */
+
+import {
+  request,
+  IRequestOptions,
+  getPortalUrl
+} from "@esri/arcgis-rest-request";
+
+import { UserSession } from "@esri/arcgis-rest-auth";
+
+// import { getUser } from "@esri/arcgis-rest-users";
+
+export interface ISetAccessRequestOptions extends IRequestOptions {
+  /**
+   * Item identifier
+   */
+  id: string;
+  /**
+   * Item owner, if different from the authenticated user.
+   */
+  owner?: string;
+  /**
+   * "private" indicates that the item can only be accessed by the user. "public" means accessible to anyone. An item shared to the organization has an access level of "org".
+   */
+  access: "private" | "public" | "org" | "shared";
+  authentication?: UserSession;
+}
+
+export interface ISharingResponse {
+  notSharedWith: string[];
+  itemId: string;
+}
+/**
+ * Generic method for sharing stuff.
+ *
+ * ```js
+ * import { setAccess } from '@esri/arcgis-rest-sharing';
+ *
+ * setItemAccess({
+ *   id: "abc123",
+ *   access: "public"
+ * })
+ *   .then(response => // sure! });
+ * ```
+ *
+ * @param requestOptions - Options for the request.
+ * @returns A Promise that will resolve with the data from the response.
+ */
+export function setAccess(
+  requestOptions: ISetAccessRequestOptions
+): Promise<ISharingResponse> {
+  const username = requestOptions.authentication.username;
+  const owner = requestOptions.owner || username;
+  const sharingUrl = `${getPortalUrl(
+    requestOptions
+  )}/content/users/${encodeURIComponent(owner)}/items/${
+    requestOptions.id
+  }/share`;
+  const usernameUrl = `${getPortalUrl(
+    requestOptions
+  )}/community/users/${encodeURIComponent(username)}`;
+
+  if (owner !== username) {
+    // more manual than calling out to getUser(), but one less dependency
+    return request(usernameUrl, {
+      authentication: requestOptions.authentication
+    }).then(response => {
+      if (!response.role || response.role !== "org_admin") {
+        throw Error(
+          `This item can not be shared by ${username}. They are neither the item owner nor an organization admin.`
+        );
+      } else {
+        return updateAccess(sharingUrl, requestOptions);
+      }
+    });
+  } else {
+    return updateAccess(sharingUrl, requestOptions);
+  }
+}
+
+function updateAccess(
+  url: string,
+  requestOptions: ISetAccessRequestOptions
+): Promise<any> {
+  requestOptions.params = {
+    org: false,
+    everyone: false,
+    ...requestOptions.params
+  };
+
+  // we're setting 'items' unnecessarily in: https://github.com/Esri/ember-arcgis-portal-services/blob/master/addon/services/sharing-service.js#L26
+
+  if (requestOptions.access === "org") {
+    requestOptions.params.org = true;
+    requestOptions.params.everyone = false;
+  }
+  if (requestOptions.access === "public") {
+    requestOptions.params.org = true;
+    requestOptions.params.everyone = true;
+  }
+  if (requestOptions.access === "private") {
+    requestOptions.params.groups = " ";
+  }
+
+  return request(url, requestOptions);
+}
---
+++ packages/arcgis-rest-sharing/test/mocks/sharing.ts
@@ -0,0 +1,15 @@
+import { UserSession } from "@esri/arcgis-rest-auth";
+import { TOMORROW } from "@esri/arcgis-rest-auth/test/utils";
+
+export const MOCK_USER_SESSION = new UserSession({
+  clientId: "clientId",
+  redirectUri: "https://example-app.com/redirect-uri",
+  token: "fake-token",
+  tokenExpires: TOMORROW,
+  refreshToken: "refreshToken",
+  refreshTokenExpires: TOMORROW,
+  refreshTokenTTL: 1440,
+  username: "jsmith",
+  password: "123456",
+  portal: "https://myorg.maps.arcgis.com/sharing/rest"
+});
