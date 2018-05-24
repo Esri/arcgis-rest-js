9eacb845dbbeb5e6f2431571d60a6c679cc63c47
john gravois <jgravois@esri.com>
2018-05-24 16:50:55 -0700

    it works

--- packages/arcgis-rest-sharing/src/group-sharing.ts
+++ packages/arcgis-rest-sharing/src/group-sharing.ts
@@ -7,13 +7,20 @@ import {
   getPortalUrl
 } from "@esri/arcgis-rest-request";
 
-import { ISharingRequestOptions, ISharingResponse } from "./helper";
+import {
+  ISharingRequestOptions,
+  ISharingResponse,
+  isAdmin,
+  getUserMembership,
+  getOwner
+} from "./helper";
 
 export interface IGroupSharingRequestOptions extends ISharingRequestOptions {
   /**
    * Group identifier
    */
   groupId: string;
+  confirmItemControl?: boolean;
 }
 
 interface IGroupSharingUnsharingRequestOptions
@@ -72,8 +79,86 @@ export function unshareItemWithGroup(
 function changeGroupSharing(
   requestOptions: IGroupSharingUnsharingRequestOptions
 ): Promise<ISharingResponse> {
-  // https://github.com/Esri/ember-arcgis-portal-services/blob/master/addon/services/sharing-service.js
-  return request("http://google.com", requestOptions);
+  const username = requestOptions.authentication.username;
+  const owner = requestOptions.owner || username;
+
+  return isAdmin(requestOptions).then(admin => {
+    const resultProp =
+      requestOptions.action === "share" ? "notSharedWith" : "notUnsharedFrom";
+    // check if the item is already shared with group...
+    return isItemSharedWithGroup(requestOptions).then(result => {
+      // if we are sharing and result is true OR we are unsharing and result is false... short circuit
+      if (
+        (requestOptions.action === "share" && result === true) ||
+        (requestOptions.action === "unshare" && result === false)
+      ) {
+        // item is shared so we can short-circuit here and send back the same structure ago would
+        const obj = { itemId: requestOptions.id, shortcut: true } as any;
+        obj[resultProp] = [];
+        return obj;
+      } else {
+        // is the user a member of the group?
+        return getUserMembership(requestOptions)
+          .then(membership => {
+            if (!membership) {
+              // reject the whole thing...
+              throw Error(
+                `This item can not be shared by ${username} as they are not a member of the specified group ${
+                  requestOptions.groupId
+                }.`
+              );
+            } else {
+              // user is a member of the group - now we figure out if/how they can share it...
+              // if user is the owner, or orgAdmin, they can share to the group using the item-owner url...
+              if (requestOptions.owner && requestOptions.owner === username) {
+                return `${getPortalUrl(
+                  requestOptions
+                )}/content/users/${owner}/items/${requestOptions.id}/${
+                  requestOptions.action
+                }`;
+              } else {
+                if (membership === "admin" || admin) {
+                  return `${getPortalUrl(requestOptions)}/content/items/${
+                    requestOptions.id
+                  }/${requestOptions.action}`;
+                } else {
+                  // user can not share item to group b/c they don't own the item
+                  throw Error(
+                    `This item can not be ${
+                      requestOptions.action
+                    } by ${username} as they are neither the owner, a groupAdmin of ${
+                      requestOptions.groupId
+                    }, nor an org_admin.`
+                  );
+                }
+              }
+            }
+          })
+          .then(urlPath => {
+            // actually do the sharing...
+            requestOptions.params = {
+              groups: requestOptions.groupId,
+              confirmItemControl: requestOptions.confirmItemControl
+            };
+            // if we mixed in, the old query parameters would be passed through
+
+            return request(urlPath, requestOptions);
+          })
+          .then(sharingResult => {
+            if (sharingResult[resultProp].length) {
+              throw Error(
+                `Item ${requestOptions.id} could not be ${
+                  requestOptions.action
+                }ed to group ${requestOptions.groupId}.`
+              );
+            } else {
+              // all is well
+              return sharingResult;
+            }
+          });
+      } // else
+    }); // then
+  });
 }
 
 /**
@@ -93,10 +178,10 @@ function isItemSharedWithGroup(
   };
 
   requestOptions.params = {
-    query,
+    ...query,
     ...requestOptions.params
   };
-
+  // more manual than calling out to "@esri/arcgis-rest-items, but trims a dependency
   const url = `${getPortalUrl(requestOptions)}/search`;
   return request(url, requestOptions).then(searchResult => {
     if (searchResult.total === 0) {
--- packages/arcgis-rest-sharing/src/helper.ts
+++ packages/arcgis-rest-sharing/src/helper.ts
@@ -8,6 +8,8 @@ import {
 
 import { UserSession } from "@esri/arcgis-rest-auth";
 
+import { IGroupSharingRequestOptions } from "./group-sharing";
+
 export interface ISharingRequestOptions extends IRequestOptions {
   /**
    * Item identifier
@@ -33,6 +35,10 @@ export function getSharingUrl(requestOptions: ISharingRequestOptions): string {
   )}/items/${requestOptions.id}/share`;
 }
 
+export function getOwner(requestOptions: ISharingRequestOptions): string {
+  return requestOptions.owner || requestOptions.authentication.username;
+}
+
 export function isItemOwner(requestOptions: ISharingRequestOptions): boolean {
   const username = requestOptions.authentication.username;
   const owner = requestOptions.owner || username;
@@ -42,7 +48,7 @@ export function isItemOwner(requestOptions: ISharingRequestOptions): boolean {
 export function isAdmin(
   requestOptions: ISharingRequestOptions
 ): Promise<boolean> {
-  // more manual than calling out to "@esri/arcgis-rest-users, but one less dependency
+  // more manual than calling out to "@esri/arcgis-rest-users, but trims a dependency
   const username = requestOptions.authentication.username;
   const url = `${getPortalUrl(
     requestOptions
@@ -56,4 +62,38 @@ export function isAdmin(
       return true;
     }
   });
+}
+
+export function getUserMembership(
+  requestOptions: IGroupSharingRequestOptions
+): Promise<string> {
+  let result = "nonmember";
+  const username = requestOptions.authentication.username;
+
+  const urlPath = `${getPortalUrl(requestOptions)}/community/groups/${
+    requestOptions.groupId
+  }/users?f=json`;
+
+  return request(urlPath, { authentication: requestOptions.authentication })
+    .then((response: any) => {
+      // check if username is in the admin hash...
+      if (response.owner === username) {
+        result = "owner";
+      }
+      if (response.admins.includes(username)) {
+        result = "admin";
+      }
+      if (response.users.includes(username)) {
+        result = "user";
+      }
+      return result;
+    })
+    .catch(err => {
+      // debug(`GroupService:getUserMembership ${id} for ${username} errored: ${err}`);
+      throw Error(
+        `GroupService:getUserMembership ${
+          requestOptions.id
+        } for ${username} errored: ${err}`
+      );
+    });
 }
