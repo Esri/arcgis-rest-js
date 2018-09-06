377c54a50e06436a20277969783973fd27dd7bf1
Mike Tschudi <mtschudi@esri.com>
2018-09-06 14:29:45 -0700

    Initial tests

---
+++ packages/arcgis-rest-items/test/mocks/featureService.ts
@@ -0,0 +1,20 @@
+/* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
+ * Apache-2.0 */
+
+export const FeatureServiceSuccessResponse: any = {
+  encodedServiceURL:
+    "https://services.arcgis.com/b6gLrKHqgkQb393u/arcgis/rest/services/EmptyServiceName/FeatureServer",
+  isView: false,
+  itemId: "1b1a3c914ef94c49ae55ce223cac5754",
+  name: "EmptyServiceName",
+  serviceItemId: "1b1a3c914ef94c49ae55ce223cac5754",
+  serviceurl:
+    "https://services.arcgis.com/b6gLrKHqgkQb393u/arcgis/rest/services/EmptyServiceName/FeatureServer",
+  size: -1,
+  success: true,
+  type: "Feature Service"
+};
+
+export const FeatureServiceFailResponse: any = {
+  success: false
+};
