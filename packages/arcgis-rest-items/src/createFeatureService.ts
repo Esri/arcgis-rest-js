f84089f2fd6ac631227d37ac9ac81b93042b9d8e
Mike Tschudi <mtschudi@esri.com>
2018-09-06 11:22:36 -0700

    Updated types and de-footgunned folder parameter

--- packages/arcgis-rest-items/src/createFeatureService.ts
+++ packages/arcgis-rest-items/src/createFeatureService.ts
@@ -2,9 +2,71 @@
  * Apache-2.0 */
 
 import { request, getPortalUrl } from "@esri/arcgis-rest-request";
-import { IItemAdd } from "@esri/arcgis-rest-common-types";
 import { moveItem } from "./update";
 import { IItemCrudRequestOptions, determineOwner } from "./helpers";
+import { IExtent, ISpatialReference } from "@esri/arcgis-rest-common-types";
+
+/**
+ * A [`createParameters` JSON object for a new
+ * service](https://developers.arcgis.com/rest/users-groups-and-items/create-service.htm).
+ */
+export interface IFeatureServiceAdd {
+  /**
+   * Name of the service to be created. This name must be unique. If the name already exists, the operation will fail. ArcGIS Enterprise does not allow spaces or special characters other than underscores in a service name.
+   */
+  name?: string;
+  /**
+   * Description given to the service.
+   */
+  serviceDescription?: string;
+  /**
+   * Indicates whether the data changes.
+   */
+  hasStaticData?: boolean;
+  /**
+   * A double value indicating any constraints enforced on query operations.
+   */
+  maxRecordCount?: number;
+  /**
+   * The formats in which query results are returned.
+   */
+  supportedQueryFormats?: string;
+  /**
+   * Specify feature service editing capabilities for Create, Delete, Query, Update, and Sync.
+   */
+  capabilities?: string;
+  /**
+   * A user-friendly description for the published dataset.
+   */
+  description?: string;
+  /**
+   * Copyright information associated with the dataset.
+   */
+  copyrightText?: string;
+  /**
+   * All layers added to a hosted feature service need to have the same spatial reference defined for
+   * the feature service. When creating a new empty service without specifying its spatial reference,
+   * the spatial reference of the hosted feature service is set to the first layer added to that
+   * feature service.
+   */
+  spatialReference?: ISpatialReference;
+  /**
+   * The initial extent set for the service.
+   */
+  initialExtent?: IExtent;
+  /**
+   * Indicates if updating the geometry of the service is permitted.
+   */
+  allowGeometryUpdates?: boolean;
+  /**
+   * Units used by the feature service
+   */
+  units?: string;
+  /**
+   * A JSON object specifying the properties of cross-site scripting prevention.
+   */
+  xssPreventionInfo?: any;
+}
 
 export interface IAddFeatureServiceRequestOptions
   extends IItemCrudRequestOptions {
@@ -13,7 +75,12 @@ export interface IAddFeatureServiceRequestOptions
    * Documentation](https://developers.arcgis.com/rest/users-groups-and-items/working-with-users-groups-and-items.htm)
    * for more information.
    */
-  item: IItemAdd;
+  item: IFeatureServiceAdd;
+  /**
+   * Alphanumeric id of folder to house moved item. If null, empty, or "/", the destination is the
+   * root folder.
+   */
+  folderId?: string;
 }
 
 export interface IAddFeatureServiceResult {
@@ -122,7 +189,7 @@ export function createFeatureService(
     ...requestOptions.params
   };
 
-  if (!requestOptions.folder || requestOptions.folder === "/") {
+  if (!requestOptions.folderId || requestOptions.folderId === "/") {
     // If the service is destined for the root folder, just send the request and return the promise;
     // services are created in the root folder
     return request(url, requestOptions);
@@ -133,7 +200,7 @@ export function createFeatureService(
       request(url, requestOptions).then(results => {
         moveItem({
           itemId: results.itemId,
-          folder: requestOptions.folder,
+          folder: requestOptions.folderId,
           authentication: requestOptions.authentication
         }).then(
           () => {
