35595ad21da35d86f4f1c9d50719646bcd13bda2
john gravois <jgravois@esri.com>
2018-11-14 13:00:44 -0800

    rename getFeatureService to getLayer

--- packages/arcgis-rest-feature-service/test/getLayer.test.ts
+++ packages/arcgis-rest-feature-service/test/getLayer.test.ts
@@ -1,13 +1,13 @@
 /* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-import { getFeatureService } from "../src/getFeatureService";
+import { getLayer } from "../src/getLayer";
 
 import * as fetchMock from "fetch-mock";
 
 import { getFeatureServiceResponse } from "./mocks/service";
 
-const serviceUrl =
+const layerUrl =
   "https://services.arcgis.com/f8b/arcgis/rest/services/Custom/FeatureServer/0";
 
 describe("feature", () => {
@@ -15,11 +15,11 @@ describe("feature", () => {
 
   it("should fetch service metadata", done => {
     fetchMock.once("*", getFeatureServiceResponse);
-    getFeatureService(serviceUrl)
+    getLayer(layerUrl)
       .then(response => {
         expect(fetchMock.called()).toBeTruthy();
         const [url, options]: [string, RequestInit] = fetchMock.lastCall("*");
-        expect(url).toEqual(serviceUrl);
+        expect(url).toEqual(layerUrl);
         expect(options.method).toBe("POST");
         expect(response).toEqual(getFeatureServiceResponse);
         done();
