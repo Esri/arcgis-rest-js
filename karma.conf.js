636a3886834f20507982315c0236f5754c2ae8be
john gravois <jgravois@esri.com>
2019-01-16 22:48:31 -0800

    codecov yml

--- karma.conf.js
+++ karma.conf.js
@@ -55,12 +55,6 @@ module.exports = function(config) {
       }
     },
 
-    // coveralls uses this one. still need to figure out how to DRY this up.
-    // coverageReporter: { 
-    //   type: 'lcov',
-    //   dir: 'coverage/'
-    // },
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
@@ -70,7 +64,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["jasmine-diff", "dots", "karma-typescript", "coverage", "coveralls"],
+    reporters: ["jasmine-diff", "dots", "karma-typescript"],
 
     // web server port
     port: 9876,
