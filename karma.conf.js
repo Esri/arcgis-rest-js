a648de67cc3f39692666d9c271ac2cca3d89b9e9
john gravois <jgravois@esri.com>
2019-01-16 16:03:31 -0800

    :see_no_evil:

--- karma.conf.js
+++ karma.conf.js
@@ -44,10 +44,10 @@ module.exports = function(config) {
     },
 
     // coveralls uses this one. still need to figure out how to DRY this up.
-    coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/'
-    },
+    // coverageReporter: {
+    //   type: 'lcov',
+    //   dir: 'coverage/'
+    // },
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -58,7 +58,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["jasmine-diff", "dots", "karma-typescript", "coverage", "coveralls"],
+    reporters: ["jasmine-diff", "dots", "karma-typescript"],
 
     // web server port
     port: 9876,
