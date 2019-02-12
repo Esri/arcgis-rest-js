7c149e7fba8d4322dcde616f06662fd932562e46
john gravois <jgravois@esri.com>
2019-02-12 15:20:05 -0800

    :nail_care:

--- packages/arcgis-rest-items/test/mocks/item.ts
+++ packages/arcgis-rest-items/test/mocks/item.ts
@@ -2,9 +2,7 @@
  * Apache-2.0 */
 
 import { IItem } from "@esri/arcgis-rest-common-types";
-import { IItemGroupResponse } from "../../src/get";
-
-import { IGetRelatedItemsResponse } from "../../src/get"
+import { IGetRelatedItemsResponse, IItemGroupResponse } from "../../src/get";
 
 export const ItemSuccessResponse: any = {
   success: true,
@@ -33,7 +31,7 @@ export const ItemResponse: IItem = {
 export const RelatedItemsResponse: IGetRelatedItemsResponse = {
   total: 1,
   relatedItems: [ItemResponse]
-}
+};
 
 export const ItemDataResponse: any = {
   source: "3ef",
@@ -43,83 +41,79 @@ export const ItemDataResponse: any = {
 };
 
 export const ItemGroupResponse: IItemGroupResponse = {
-  "admin": [{
-    "id": "2ecb37a8c8fb4051af9c086c25503bb0",
-    "title": "Street Maps",
-    "isInvitationOnly": false,
-    "owner": "jsmith",
-    "description": "The street maps group provides street maps for the city of Redlands.",
-    "snippet": null,
-    "tags": [
-      "Redlands",
-      "Street",
-      "Maps"
-    ],
-    "phone": "909.555.1234",
-    "thumbnail": "streets.jpg",
-    "created": 1247082196000,
-    "modified": 1276793808000,
-    "access": "public",
-    "userMembership": {
-      "username": "jsmith",
-      "memberType": "owner",
-      "applications": 1
-    },
-    "isFav": false,
-    "autoJoin": false,
-    "isViewOnly": false,
-    "protected": false
-  }],
-  "member": [{
-    "id": "bf51aa6e879e4676b683dcbefb0ab0a9",
-    "title": "Parks and Recreation",
-    "isInvitationOnly": true,
-    "owner": "swilson",
-    "description": "The Parks and Recreation group contains maps and applications used by the Parks Department.",
-    "snippet": null,
-    "tags": [
-      "Redlands",
-      "Parks",
-      "Recreation"
-    ],
-    "phone": "909.555.1234",
-    "thumbnail": "parks.jpg",
-    "created": 1247082197000,
-    "modified": 1276793919000,
-    "access": "private",
-    "userMembership": {
-      "username": "jsmith",
-      "memberType": "member",
-    },
-    "isFav": false,
-    "autoJoin": false,
-    "isViewOnly": false,
-    "protected": false
-  }],
-  "other":[{
-    "id": "dbc385ac1b7d4231b24b97750f0e633c",
-    "title": "Featured Maps and Apps",
-    "isInvitationOnly": true,
-    "owner": "city_redlands",
-    "description": "These items are featured on the gallery page.",
-    "snippet": null,
-    "tags": [
-      "gallery",
-      "Featured Maps",
-      "Featured Apps"
-    ],
-    "phone": "909.555.1234",
-    "thumbnail": "gallery.jpg",
-    "created": 1327099662000,
-    "modified": 1327099662000,
-    "access": "public",
-    "userMembership": {
-      "username": "jsmith",
-      "memberType": "nonmember"
-    },
-    "isFav": false,
-    "autoJoin": false,
-    "isViewOnly": false,
-    "protected": false
-  }]
-}
+  admin: [
+    {
+      id: "2ecb37a8c8fb4051af9c086c25503bb0",
+      title: "Street Maps",
+      isInvitationOnly: false,
+      owner: "jsmith",
+      description:
+        "The street maps group provides street maps for the city of Redlands.",
+      snippet: null,
+      tags: ["Redlands", "Street", "Maps"],
+      phone: "909.555.1234",
+      thumbnail: "streets.jpg",
+      created: 1247082196000,
+      modified: 1276793808000,
+      access: "public",
+      userMembership: {
+        username: "jsmith",
+        memberType: "owner",
+        applications: 1
+      },
+      isFav: false,
+      autoJoin: false,
+      isViewOnly: false,
+      protected: false
+    }
+  ],
+  member: [
+    {
+      id: "bf51aa6e879e4676b683dcbefb0ab0a9",
+      title: "Parks and Recreation",
+      isInvitationOnly: true,
+      owner: "swilson",
+      description:
+        "The Parks and Recreation group contains maps and applications used by the Parks Department.",
+      snippet: null,
+      tags: ["Redlands", "Parks", "Recreation"],
+      phone: "909.555.1234",
+      thumbnail: "parks.jpg",
+      created: 1247082197000,
+      modified: 1276793919000,
+      access: "private",
+      userMembership: {
+        username: "jsmith",
+        memberType: "member"
+      },
+      isFav: false,
+      autoJoin: false,
+      isViewOnly: false,
+      protected: false
+    }
+  ],
+  other: [
+    {
+      id: "dbc385ac1b7d4231b24b97750f0e633c",
+      title: "Featured Maps and Apps",
+      isInvitationOnly: true,
+      owner: "city_redlands",
+      description: "These items are featured on the gallery page.",
+      snippet: null,
+      tags: ["gallery", "Featured Maps", "Featured Apps"],
+      phone: "909.555.1234",
+      thumbnail: "gallery.jpg",
+      created: 1327099662000,
+      modified: 1327099662000,
+      access: "public",
+      userMembership: {
+        username: "jsmith",
+        memberType: "nonmember"
+      },
+      isFav: false,
+      autoJoin: false,
+      isViewOnly: false,
+      protected: false
+    }
+  ]
+};
