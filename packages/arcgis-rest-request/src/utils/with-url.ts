38b3500c1473eb20f86e5b990a8d1a329bcae653
Patrick Arlt <patrick.arlt@gmail.com>
2019-04-10 17:19:48 -0700

    use withOptions for withUrl

--- packages/arcgis-rest-request/src/utils/with-url.ts
+++ packages/arcgis-rest-request/src/utils/with-url.ts
@@ -1,18 +1,9 @@
 import { IRequestOptions } from "./IRequestOptions";
+import { withOptions } from "./with-options";
 
 export function withUrl<T extends (...args: any[]) => any>(
   func: T,
   url: string
 ): (...funcArgs: Parameters<T>) => ReturnType<T> {
-  return (...args: Parameters<T>): ReturnType<T> => {
-    const options: IRequestOptions =
-      typeof args[args.length - 1] === "object"
-        ? {
-            ...{ url },
-            ...args.pop()
-          }
-        : { url };
-
-    return func(...[...args, options]);
-  };
+  return withOptions(func, { url });
 }
