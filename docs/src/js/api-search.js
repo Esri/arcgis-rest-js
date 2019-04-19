79a05e93f78feaf900ffd0adb14d0132ea6acc41
Patrick Arlt <patrick.arlt@gmail.com>
2019-04-19 09:54:15 -0700

    more style tweaks calcite web upgrade

--- docs/src/js/api-search.js
+++ docs/src/js/api-search.js
@@ -1,7 +1,7 @@
 Vue.component("api-search", {
   template: `
     <div>
-      <form v-on:submit.prevent="onSubmit" class="input-group trailer-quarter leader-0  ">
+      <form v-on:submit.prevent="onSubmit" class="input-group trailer-half leader-0">
         <input ref="input" v-model="searchTerm" v-on:keyup.enter="onSubmit" v-on:keyup.up="onSelectPrevious" v-on:keyup.down="onSelectNext" v-on:keyup="onChange" class="input-group-input" type="text" placeholder="Search the API Reference">
         <span class="input-group-button">
           <button type="submit" class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" class="svg-icon"><path d="M31.607 27.838l-6.133-6.137a1.336 1.336 0 0 0-1.887 0l-.035.035-2.533-2.533-.014.014c3.652-4.556 3.422-11.195-.803-15.42-4.529-4.527-11.875-4.531-16.404 0-4.531 4.531-4.529 11.875 0 16.406 4.205 4.204 10.811 4.455 15.365.848l.004.003-.033.033 2.541 2.54a1.33 1.33 0 0 0 .025 1.848l6.135 6.133a1.33 1.33 0 0 0 1.887 0l1.885-1.883a1.332 1.332 0 0 0 0-1.887zM17.811 17.809a8.213 8.213 0 0 1-11.619 0 8.217 8.217 0 0 1 0-11.622 8.219 8.219 0 0 1 11.619.004 8.216 8.216 0 0 1 0 11.618z"/></svg></button>
