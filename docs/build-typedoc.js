a3c605d4923d78ce99c34f6f2c72e96964d45b8e
Patrick Arlt <patrickarlt@users.noreply.github.com>
2019-04-19 17:13:41 -0700

    Doc new methods for v2.0.0 (#534)
    * SearchQueryBuilder docs
    
    * more docs for defaults and withOptions
    
    * :nail_care:

--- docs/build-typedoc.js
+++ docs/build-typedoc.js
@@ -23,7 +23,8 @@ const md = new MarkdownIt();
         "--module",
         "common",
         "--tsconfig",
-        "./tsconfig.json"
+        "./tsconfig.json",
+        "--excludePrivate"
       ],
       {
         stdio: "inherit"
