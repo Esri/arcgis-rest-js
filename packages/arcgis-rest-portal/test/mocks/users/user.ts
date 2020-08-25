e39eab15d2edd0158fb0327c631209eb49cb750b
Randy Weber <rweber@esri.com>
2020-08-24 22:59:23 -0400

    Add removeGroupUsers. Refactor shareItemWithGroup and unshareItemWithGroup to unlock additional sharing capabilities natively supported by the REST API. Admin users temporarily join groups when necessary to perform shares, then get removed if added.

--- packages/arcgis-rest-portal/test/mocks/users/user.ts
+++ packages/arcgis-rest-portal/test/mocks/users/user.ts
@@ -109,7 +109,7 @@ export const GroupNonMemberUserResponse: IUser = {
   ]
 };
 
-export const GroupAdminUserResponse = {
+export const GroupAdminUserResponse: IUser = {
   ...GroupMemberUserResponse,
   groups: [
     {
@@ -140,7 +140,7 @@ export const GroupAdminUserResponse = {
   ]
 };
 
-export const OrgAdminUserResponse = {
+export const OrgAdminUserResponse: IUser = {
   ...GroupAdminUserResponse,
   role: "org_admin",
   groups: [
