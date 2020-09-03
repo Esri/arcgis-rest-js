e97c880c0066e6c4530cc7c4fe93991a693d855b
Randy Weber <52967037+rweber-esri@users.noreply.github.com>
2020-09-03 11:41:13 -0400

    Add removeGroupUsers. Refactor shareItemWithGroup and unshareItemWith… (#737)
    * Add removeGroupUsers. Refactor shareItemWithGroup and unshareItemWithGroup to unlock additional sharing capabilities natively supported by the REST API. Admin users temporarily join groups when necessary to perform shares, then get removed if added.
    
    * Fix logic that detects org admins, optimize share-item-with-group calls, minor cleanup, test updates
    
    * Fix admin detection in unshare-item-with-group, minor cleanup

---
+++ packages/arcgis-rest-portal/src/groups/remove-users.ts
@@ -0,0 +1,76 @@
+/* Copyright (c) 2017-2018 Environmental Systems Research Institute, Inc.
+ * Apache-2.0 */
+
+import {
+  request,
+  IRequestOptions,
+  ArcGISRequestError
+} from "@esri/arcgis-rest-request";
+import { getPortalUrl } from "../util/get-portal-url";
+import { chunk } from "../util/array";
+
+export interface IRemoveGroupUsersOptions extends IRequestOptions {
+  /**
+   * Group ID
+   */
+  id: string;
+  /**
+   * An array of usernames to be removed from the group
+   */
+  users?: string[];
+}
+
+export interface IRemoveGroupUsersResult {
+  /**
+   * An array of usernames that were not removed
+   */
+  notRemoved?: string[];
+  /**
+   * An array of request errors
+   */
+  errors?: ArcGISRequestError[];
+}
+
+/**
+ * ```js
+ * import { removeGroupUsers } from "@esri/arcgis-rest-portal";
+ * //
+ * removeGroupUsers({
+ *   id: groupId,
+ *   users: ["username1", "username2"],
+ *   authentication
+ * })
+ * .then(response);
+ * ```
+ * Add users to a group. See the [REST Documentation](https://developers.arcgis.com/rest/users-groups-and-items/remove-users-from-group.htm) for more information.
+ *
+ * @param requestOptions  - Options for the request
+ * @returns A Promise
+ */
+export function removeGroupUsers (
+  requestOptions: IRemoveGroupUsersOptions
+): Promise<IRemoveGroupUsersResult> {
+  const { id, users: usersToRemove } = requestOptions;
+  const url = `${getPortalUrl(requestOptions)}/community/groups/${id}/removeUsers`;
+  const safeSend = (users: string[]) => {
+    const options: IRemoveGroupUsersOptions = {
+      ...requestOptions,
+      users,
+      params: { users }
+    };
+    return request(url, options)
+      .catch(error => ({ errors: [error] }));
+  };
+  // the ArcGIS REST API only allows to add no more than 25 users per request,
+  // see https://developers.arcgis.com/rest/users-groups-and-items/remove-users-from-group.htm
+  const promises = chunk(usersToRemove, 25).map(usersChunk => safeSend(usersChunk));
+  return Promise.all(promises)
+    .then(results => {
+      const filtered = (propName: string) => results
+        .filter(result => result[propName])
+        .reduce((collection, result) => collection.concat(result[propName]), []);
+      const errors = filtered('errors');
+      const consolidated: IRemoveGroupUsersResult = { notRemoved: filtered('notRemoved') };
+      return errors.length ? { ...consolidated, errors } : consolidated;
+    });
+}
---
+++ packages/arcgis-rest-portal/src/sharing/unshare-item-with-group.ts
@@ -0,0 +1,105 @@
+import { request } from "@esri/arcgis-rest-request";
+import { getPortalUrl } from "../util/get-portal-url";
+import {
+  IGroupSharingOptions,
+  ISharingResponse,
+  getUserMembership
+} from "./helpers";
+import { isItemSharedWithGroup } from "./is-item-shared-with-group";
+import { getUser } from "../users/get-user";
+
+/**
+ * Stop sharing an item with a group, either as an
+ * [item owner](https://developers.arcgis.com/rest/users-groups-and-items/unshare-item-as-item-owner-.htm),
+ * [group admin](https://developers.arcgis.com/rest/users-groups-and-items/unshare-item-as-group-admin-.htm) or
+ * organization admin.
+ *
+ * ```js
+ * import { unshareItemWithGroup } from '@esri/arcgis-rest-portal';
+ *
+ * unshareItemWithGroup({
+ *   id: "abc123",
+ *   groupId: "xyz987",
+ *   owner: "some-owner",
+ *   authentication: session
+ * })
+ * ```
+ *
+ * @param requestOptions - Options for the request.
+ * @returns A Promise that will resolve with the data from the response.
+ */
+export function unshareItemWithGroup (
+  requestOptions: IGroupSharingOptions
+): Promise<ISharingResponse> {
+  return isItemSharedWithGroup(requestOptions)
+    .then(isShared => {
+      // not shared
+      if (!isShared) {
+        // exit early with success response
+        return Promise.resolve({
+          itemId: requestOptions.id,
+          shortcut: true,
+          notUnsharedFrom: []
+        } as ISharingResponse);
+      }
+
+      const {
+        authentication: { username },
+        owner
+      } = requestOptions;
+
+      // next check if the user is a member of the group
+      return Promise.all([
+        getUserMembership(requestOptions),
+        getUser({
+          username,
+          authentication: requestOptions.authentication
+        })
+      ])
+        .then(([membership, currentUser]) => {
+          const itemOwner = owner || username;
+          const isItemOwner = itemOwner === username;
+          const isAdmin = currentUser.role === 'org_admin' && !currentUser.roleId;
+
+          if (!isItemOwner && !isAdmin && ['admin', 'owner'].indexOf(membership) < 0) {
+            // abort and reject promise
+            throw Error(`This item can not be unshared from group ${requestOptions.groupId} by ${username} as they not the item owner, an org admin, group admin or group owner.`);
+          }
+          
+          // let the sharing call go
+          return unshareFromGroup(requestOptions);
+        })
+        .then(sharingResponse => {
+          if (sharingResponse.notUnsharedFrom.length) {
+            throw Error(
+              `Item ${requestOptions.id} could not be unshared to group ${requestOptions.groupId}`
+            );
+          } else {
+            // all is well
+            return sharingResponse;
+          }
+        });
+  });
+}
+
+function unshareFromGroup (
+  requestOptions: IGroupSharingOptions
+): Promise<ISharingResponse> {
+  const username = requestOptions.authentication.username;
+  const itemOwner = requestOptions.owner || username;
+  // decide what url to use
+  // default to the non-owner url...
+  let url = `${getPortalUrl(requestOptions)}/content/items/${requestOptions.id}/unshare`;
+
+  // but if they are the owner, we use a different path...
+  if (itemOwner === username) {
+    url = `${getPortalUrl(requestOptions)}/content/users/${itemOwner}/items/${requestOptions.id}/unshare`;
+  }
+
+  // now its finally time to do the sharing
+  requestOptions.params = {
+    groups: requestOptions.groupId
+  };
+
+  return request(url, requestOptions);
+}
