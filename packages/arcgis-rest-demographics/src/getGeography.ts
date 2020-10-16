43b2d88face73c390d9510d2e1eea1d2b9d671b5
Gavin Rehkemper <grehkemper@esri.com>
2020-10-16 14:17:50 -0500

    wording changes for doc site

--- packages/arcgis-rest-demographics/src/getGeography.ts
+++ packages/arcgis-rest-demographics/src/getGeography.ts
@@ -12,67 +12,67 @@ import { ARCGIS_ONLINE_STANDARD_GEOGRAPHY_QUERY_URL, IGeoenrichmentResult } from
 
 export interface IGetGeographyOptions extends IRequestOptions {
   /**
-   * Optional parameter to specify the source country for the search.
+   * Specify the source country for the search.
    */
   sourceCountry?: string;
   /**
-   * Optional parameter to specify a specific dataset within a defined country.
+   * Specify a specific dataset within a defined country.
    */
   optionalCountryDataset?: string;
   /**
-   * Optional parameter to specify which standard geography layers are being queried or searched.
+   * Specify which standard geography layers are being queried or searched.
    */
   geographyLayers?: string[] | string;
   /**
-   * Optional parameter to specify which IDs for the standard geography layers are being queried or searched.
+   * Specify which IDs for the standard geography layers are being queried or searched.
    */
   geographyIDs?: string[];
   /**
-   * Optional parameter to specify the text to query and search the standard geography layers specified.
+   * Specify the text to query and search the standard geography layers specified.
    */
   geographyQuery?: string;
   /**
-   * Optional parameter to return all the subgeographic areas that are within a parent geography.
+   * Return all the subgeographic areas that are within a parent geography.
    */
   returnSubGeographyLayer?: boolean;
   /**
-   * Optional parameter to return all the subgeographic areas that are within a parent geography.
+   * Return all the subgeographic areas that are within a parent geography.
    */
   subGeographyLayer?: string;
   /**
-   * Optional parameter to filter the results of the subgeography features that are returned by a search term.
+   * Filter the results of the subgeography features that are returned by a search term.
    */
   subGeographyQuery?: string;
   /**
-   * Optional parameter to request the output geometries in a specified spatial reference system.
+   * Request the output geometries in a specified spatial reference system.
    */
   outSR?: number;
   /**
-   * Optional parameter to request the output geometries in the response.
+   * Request the output geometries in the response.
    */
   returnGeometry?: boolean;
   /**
-   * Optional parameter to request the output geometry to return the center point for each feature.
+   * Request the output geometry to return the center point for each feature.
    */
   returnCentroids?: boolean;
   /**
-   * Optional integer that specifies the level of generalization or detail in the area representations of the administrative boundary or standard geographic data layers.
+   * Integer that specifies the level of generalization or detail in the area representations of the administrative boundary or standard geographic data layers.
    */
   generalizationLevel?: number;
   /**
-   * Optional parameter to define if text provided in the geographyQuery parameter should utilize fuzzy search logic.
+   * Define if text provided in the geographyQuery parameter should utilize fuzzy search logic.
    */
   useFuzzySearch?: boolean;
   /**
-   * Optional parameter that limits the number of features that are returned from the geographyQuery parameter.
+   * Limits the number of features that are returned from the geographyQuery parameter.
    */
   featureLimit?: number;
   /**
-   * Optional parameter that starts the results on the number of the records specified.
+   * Starts the results on the number of the records specified.
    */
   featureOffset?: number;
   /**
-   * Optional parameter that specifies the language you wish to receive your GeoEnrichment results.
+   * Specifies the language you wish to receive your GeoEnrichment results.
    */
   langCode?: string;
 }
