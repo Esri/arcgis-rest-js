b087de24ae6d8f41a2bb13c3c25a33943f0db2d8
Dave Bouwman <dave@davebouwman.com>
2021-01-05 15:55:10 -0700

    fix: export validateAppAccess function

--- packages/arcgis-rest-auth/src/index.ts
+++ packages/arcgis-rest-auth/src/index.ts
@@ -8,3 +8,4 @@ export * from "./generate-token";
 export * from "./authenticated-request-options";
 export { IUser } from "@esri/arcgis-rest-types";
 export * from "./app-tokens";
+export * from "./validate-app-access";
