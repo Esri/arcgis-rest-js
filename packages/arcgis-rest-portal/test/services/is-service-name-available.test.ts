dc1a2e354df0903e09360530a9d94d085281aa42
Patrick Arlt <patrick.arlt@gmail.com>
2021-09-03 06:36:46 -0700

    Update TypeDoc to use the new --package option (#904)
    * udpate
    
    * intial typedoc update
    
    * update typedoc packages
    
    * remove doc file
    
    * rewrite tests to use fetchMock

--- packages/arcgis-rest-portal/test/services/is-service-name-available.test.ts
+++ packages/arcgis-rest-portal/test/services/is-service-name-available.test.ts
@@ -1,7 +1,7 @@
 /* Copyright (c) 2018-2020 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-import * as requestModule from "@esri/arcgis-rest-request";
+import * as fetchMock from "fetch-mock";
 import { isServiceNameAvailable } from "../../src/services/is-service-name-available";
 import { UserSession } from "@esri/arcgis-rest-auth";
 import { TOMORROW } from "@esri/arcgis-rest-auth/test/utils";
@@ -21,26 +21,25 @@ describe("is-service-name-available", () => {
   });
 
   it("returns server response", () => {
-    const spy = spyOn(requestModule, "request").and.callFake(() =>
-      Promise.resolve({ available: true })
-    );
+    fetchMock.once("*", { available: true }, { method: "GET" });
+
     return isServiceNameAvailable(
       "someService",
       "Feature Service",
       MOCK_USER_SESSION
     ).then((result) => {
+      const [url]: [string, RequestInit] = fetchMock.lastCall("*");
+
       expect(result.available).toBe(true, "should return the api response");
-      expect(spy.calls.count()).toBe(1, "should make one request");
-      expect(spy.calls.argsFor(0)[0]).toBe(
+      expect(fetchMock.calls().matched.length).toBe(
+        1,
+        "should make one request"
+      );
+      expect(url).toContain(
         `${MOCK_USER_SESSION.portal}/portals/self/isServiceNameAvailable`
       );
-      const opts = spy.calls.argsFor(0)[1];
-
-      expect(opts.params.name).toBe(
-        "someService",
-        "passes in the service name"
-      );
-      expect(opts.params.type).toBe("Feature Service", "passes in the type");
+      expect(url).toContain("name=someService");
+      expect(url).toContain("type=Feature");
     });
   });
 });
