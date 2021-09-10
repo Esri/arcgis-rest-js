65c32fd90db98bb9a22478669771ef895e63624b
Tom Wayson <twayson@esri.com>
2021-09-10 13:58:20 -0700

    Revert "Update TypeDoc to use the new --package option (#904)"
    This reverts commit dc1a2e354df0903e09360530a9d94d085281aa42.

--- docs/generate-srihashes.js
+++ docs/generate-srihashes.js
@@ -1,61 +1,53 @@
 /**
  * Generate a JSON file containing hash of packages and sha384 hashes of minified umd dist files.
  */
-const { join, dirname, resolve } = require("path");
-const { readFile, writeFile } = require("fs/promises");
+const { join } = require("path");
+const { readFile, writeFile } = require("fs");
 const { generate } = require("sri-toolbox");
 const OUTPUT = join(process.cwd(), "docs", "src", `srihashes.json`);
 const version = require(join(process.cwd(), "lerna.json")).version;
-const { promisify } = require("util");
 
-const glob = promisify(require("glob"));
+const packages = [
+  "auth",
+  "feature-layer",
+  "geocoding",
+  "portal",
+  "request",
+  "routing",
+  "service-admin"
+];
 
 const promises = [];
 
-glob("packages/*/package.json")
-  .then((packages) => {
-    return Promise.all(
-      packages.map((pkgPath) => {
-        return readFile(pkgPath).then((pkg) => {
-          return JSON.parse(pkg);
-        });
-      })
-    );
-  })
-  .then((packageJsons) => {
-    return Promise.all(
-      packageJsons
-        .filter((p) => !!p.unpkg)
-        .map((pkg) => {
-          return readFile(
-            resolve(join("packages", pkg.name.replace("@esri/", "")), pkg.unpkg)
-          ).then((data) => {
-            return {
-              pkg,
-              hash: generate(
-                {
-                  algorithms: ["sha384"],
-                },
-                data
-              ),
-            };
-          });
-        })
-    );
-  })
-  .then((res) => {
-    const json = {
-      version,
-      packages: {},
-    };
-    res.forEach((r) => {
-      if (r.hash) json.packages[r.pkg.name] = r.hash;
+packages.forEach(pkg => {
+  const package = `@esri/arcgis-rest-${pkg}`;
+  const promise = new Promise((resolve, reject) => {
+    readFile(`packages/arcgis-rest-${pkg}/dist/umd/${pkg}.umd.min.js`, (err, data) => {
+      err ? resolve({
+        package,
+        hash: false
+      }) : resolve({
+        package,
+        hash: generate({
+          algorithms: ["sha384"]
+        }, data)
+      });
     });
-    writeFile(OUTPUT, JSON.stringify(json, null, "  "), "utf8", (err) => {
-      if (err) throw err;
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-    // make node happy to see `catch()`
   });
+  promises.push(promise);
+});
+
+Promise.all(promises).then((res) => {
+  const json = {
+    version,
+    packages: {}
+  };
+  res.forEach((r) => {
+    if (r.hash) json.packages[r.package] = r.hash;
+  });
+  writeFile(OUTPUT, JSON.stringify(json, null, '  '), "utf8", (err) => {
+    if (err) throw err;
+  });
+}).catch((err) => {
+  // make node happy to see `catch()`
+});
--- packages/arcgis-rest-service-admin/test/get-service-admin-info.test.ts
+++ packages/arcgis-rest-service-admin/test/get-service-admin-info.test.ts
@@ -1,14 +1,12 @@
 /* Copyright (c) 2018-2020 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-import * as fetchMock from "fetch-mock";
+import * as requestModule from "@esri/arcgis-rest-request";
 import { getServiceAdminInfo } from "../src/get-service-admin-info";
 import { TOMORROW } from "@esri/arcgis-rest-auth/test/utils";
 import { UserSession } from "@esri/arcgis-rest-auth";
 
 describe("get-service-admin-info: ", () => {
-  afterEach(fetchMock.restore);
-
   const MOCK_USER_SESSION = new UserSession({
     clientId: "clientId",
     redirectUri: "https://example-app.com/redirect-uri",
@@ -21,22 +19,17 @@ describe("get-service-admin-info: ", () => {
     password: "123456",
     portal: "https://myorg.maps.arcgis.com/sharing/rest",
   });
-
   it("makes request to the admin url", () => {
-    fetchMock.once("*", { foo: "bar" }, { method: "POST" });
-
+    const spy = spyOn(requestModule, "request").and.callFake(() =>
+      Promise.resolve({ foo: "bar" })
+    );
     return getServiceAdminInfo(
       "https://servicesqa.arcgis.com/orgid/arcgis/rest/services/mysevice/FeatureServer",
       MOCK_USER_SESSION
     ).then((result) => {
-      const [url]: [string, RequestInit] = fetchMock.lastCall("*");
-
       expect(result.foo).toBe("bar", "should return the api response");
-      expect(fetchMock.calls().matched.length).toBe(
-        1,
-        "should make one request"
-      );
-      expect(url).toBe(
+      expect(spy.calls.count()).toBe(1, "should make one request");
+      expect(spy.calls.argsFor(0)[0]).toBe(
         "https://servicesqa.arcgis.com/orgid/arcgis/rest/admin/services/mysevice/FeatureServer"
       );
     });
