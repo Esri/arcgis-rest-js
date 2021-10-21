7db59e7a88c1f8d08d0837176831f041190bdac9
Dave Bouwman <dave@davebouwman.com>
2021-10-21 17:16:41 -0600

    fix import path

--- packages/arcgis-rest-portal/test/items/search.test.ts
+++ packages/arcgis-rest-portal/test/items/search.test.ts
@@ -7,7 +7,7 @@ import { searchItems } from "../../src/items/search";
 
 import { SearchResponse, BigSearchResponse } from "../mocks/items/search";
 import { UserSession } from "@esri/arcgis-rest-auth";
-import { TOMORROW } from "@esri/arcgis-rest-auth/test/utils";
+import { TOMORROW } from "../../../arcgis-rest-auth/test/utils";
 import { SearchQueryBuilder } from "../../src/util/SearchQueryBuilder";
 
 describe("search", () => {
