1bb1a8f01efde14d8d31da344e6b62d91efa1528
Dave Bouwman <dave@davebouwman.com>
2022-02-16 16:41:30 -0700

    feat: add commitlint config and release scripts

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -20,7 +20,8 @@ const Configuration = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'type-enum': [2, 'always', 
+    ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
   },
   /*
    * Functions that return true if commitlint should ignore the given message.
