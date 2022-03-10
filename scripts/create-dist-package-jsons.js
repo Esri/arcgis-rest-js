b69f30dc12705ccd9b113924d86723f4db1a57e1
Patrick Arlt <patrick.arlt@gmail.com>
2022-03-10 13:47:59 -0800

    chore: fix for different node versions

--- scripts/create-dist-package-jsons.js
+++ scripts/create-dist-package-jsons.js
@@ -3,9 +3,10 @@
  *
  * Inspired by: https://www.sensedeep.com/blog/posts/2021/how-to-create-single-source-npm-module.html
  */
-import { writeFile, access } from "fs/promises";
+import fs from "fs";
 import { join } from "path";
 
+const { writeFile, access } = fs.promises;
 const esmBuildFolder = join(process.cwd(), "dist", "esm");
 
 access(esmBuildFolder)
@@ -21,7 +22,7 @@ access(esmBuildFolder)
 
 const cjsBuildFolder = join(process.cwd(), "dist", "cjs");
 
-access(esmBuildFolder)
+access(cjsBuildFolder)
   .then(() => {
     writeFile(
       join(cjsBuildFolder, "package.json"),
