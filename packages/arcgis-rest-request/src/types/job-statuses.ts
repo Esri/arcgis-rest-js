fa2fafa0078bbc2966425a7e8512b5b095074c40
Patrick Arlt <patrick.arlt@gmail.com>
2022-09-15 11:02:53 -0700

    chore: rename file

---
+++ packages/arcgis-rest-request/src/types/job-statuses.ts
@@ -0,0 +1,14 @@
+export enum JOB_STATUSES {
+  Success = "succeeded",
+  Failed = "failed",
+  Waiting = "waiting",
+  Cancelled = "cancelled",
+  Cancelling = "cancelling",
+  New = "new",
+  Executing = "executing",
+  Submitted = "submitted",
+  Failure = "failure",
+  TimedOut = "timed-out",
+  Error = "error",
+  Status = "status"
+}
