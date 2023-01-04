afc4a5c4def0ca371c4bf005288402bed8e44668
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2023-01-04 16:28:35 -0500

    fix(arcgis-rest-portal): update tests again
    AFFECTS PACKAGES:
    @esri/arcgis-rest-portal

--- packages/arcgis-rest-portal/test/users/get-user-properties.test.ts
+++ packages/arcgis-rest-portal/test/users/get-user-properties.test.ts
@@ -4,13 +4,12 @@
 import { getUserProperties } from '../../src/users/get-user-properties'
 import { UserPropertiesResponse } from '../mocks/users/user-properties'
 import * as fetchMock from "fetch-mock";
-import { encodeParam } from '@esri/arcgis-rest-request';
 
 describe("users", () => {
   afterEach(fetchMock.restore);
 
   describe("getUserProperties", () => {
-    it("should make an authenticated request for user properties", done => {
+    it("should make a request for user properties", done => {
       fetchMock.once("*", UserPropertiesResponse);
 
       getUserProperties("c@sey")
@@ -18,9 +17,9 @@ describe("users", () => {
           expect(fetchMock.called()).toEqual(true);
           const [url, options]: [string, RequestInit] = fetchMock.lastCall("*");
           expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/properties"
+            "https://www.arcgis.com/sharing/rest/community/users/c%40sey/properties"
           );
-          expect(options.method).toBe("GET");
+          expect(options.method).toBe("POST");
           done();
         })
         .catch(e => {
