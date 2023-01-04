f913e0a42a8919c691802a3cbf01bdb135db7552
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2023-01-04 12:27:49 -0500

    feat(arcgis-rest-portal): add get user properties function
    AFFECTS PACKAGES:
    @esri/arcgis-rest-portal

---
+++ packages/arcgis-rest-portal/test/mocks/users/user-properties.ts
@@ -0,0 +1,5 @@
+import { IGetUserPropertiesResponse } from "../../../src/users/get-user-properties";
+
+export const UserPropertiesResponse: IGetUserPropertiesResponse = {
+  mapViewer: 'classic'
+};
