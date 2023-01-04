bbc47893e1e352bb795bca5fc1b44c46a43fb378
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2023-01-04 16:52:27 -0500

    fix(arcgis-rest-portal): update tests even more

--- packages/arcgis-rest-portal/test/users/get-user-properties.test.ts
+++ packages/arcgis-rest-portal/test/users/get-user-properties.test.ts
@@ -1,6 +1,7 @@
 /* Copyright (c) 2023 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
+import { IRequestOptions } from "@esri/arcgis-rest-request";
 import { getUserProperties } from '../../src/users/get-user-properties'
 import { UserPropertiesResponse } from '../mocks/users/user-properties'
 import * as fetchMock from "fetch-mock";
@@ -12,14 +13,15 @@ describe("users", () => {
     it("should make a request for user properties", done => {
       fetchMock.once("*", UserPropertiesResponse);
 
-      getUserProperties("c@sey")
+      let options = { httpMethod: "GET" } as IRequestOptions;
+      getUserProperties("c@sey", options)
         .then(() => {
           expect(fetchMock.called()).toEqual(true);
           const [url, options]: [string, RequestInit] = fetchMock.lastCall("*");
           expect(url).toEqual(
             "https://www.arcgis.com/sharing/rest/community/users/c%40sey/properties"
           );
-          expect(options.method).toBe("POST");
+          expect(options.method).toBe("GET");
           done();
         })
         .catch(e => {
