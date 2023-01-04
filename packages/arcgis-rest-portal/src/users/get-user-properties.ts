6c0e733bd3126346fafe4c114fef1eeaf4fffb3a
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2023-01-04 14:17:28 -0500

    feat(arcgis-rest-portal): implement PR feedback
    AFFECTS PACKAGES:
    @esri/arcgis-rest-portal

--- packages/arcgis-rest-portal/src/users/get-user-properties.ts
+++ packages/arcgis-rest-portal/src/users/get-user-properties.ts
@@ -1,24 +1,41 @@
 /* Copyright (c) 2023 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-import { request } from "@esri/arcgis-rest-request";
+import { IRequestOptions, request } from "@esri/arcgis-rest-request";
 import { getPortalUrl } from "../util/get-portal-url";
-import { IGetUserOptions } from "./get-user";
+import { UserSession } from "@esri/arcgis-rest-auth";
+
+export interface IGetUserPropertiesOptions extends IRequestOptions {
+  /**
+   * A session representing a logged in user.
+   */
+  authentication?: UserSession;
+  /**
+   * Supply a username if you'd like to fetch information about a different user than is being used to authenticate the request.
+   */
+  username?: string;
+}
 
 export interface IGetUserPropertiesResponse {
   /**
+   * user landing page configuration
+   */
+  landingPage: {
+    url: string
+  }
+  /**
    * user MapViewer configuration
    */
-  mapViewer: string;
+  mapViewer: "classic" | "modern"
 }
 
 /**
  * Helper that returns the properties attribute for a user.
  *
- * @param IGetUserOptions - options to pass through in the request
+ * @param IGetUserPropertiesOptions - options to pass through in the request
  * @returns User properties object
  */
-export function getUserProperties(requestOptions: IGetUserOptions): Promise<IGetUserPropertiesResponse> {
+export function getUserProperties(requestOptions: IGetUserPropertiesOptions): Promise<IGetUserPropertiesResponse> {
   const username =
     requestOptions.username || requestOptions.authentication.username;
   const url = `${getPortalUrl(requestOptions)}/community/users/${encodeURIComponent(username)}/properties`;
