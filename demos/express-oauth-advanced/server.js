7239697cc18b6c82fc5d74aa7e18a2d53e743d7a
Patrick Arlt <patrick.arlt@gmail.com>
2023-03-09 15:57:49 -0800

    chore: update advanced oAuth demo for dev summit (#1086)
    * chore: update advanced oAuth demo for dev summit
    
    * chore: handle no webmaps + typo fix
    
    ---------
    
    Co-authored-by: Noah Mulfinger <nmulfinger@esri.com>

--- demos/express-oauth-advanced/server.js
+++ demos/express-oauth-advanced/server.js
@@ -1,56 +1,64 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
 import express from "express";
 import { ArcGISIdentityManager } from "@esri/arcgis-rest-request";
 import session from "express-session";
 import SessionFileStore from "session-file-store";
 import { readFile } from "fs/promises";
 import { randomBytes } from "crypto";
+import path from "path";
 
-/**
- * Create a new express and file store for our sessions
- */
-const app = express();
+dotenv.config();
+
+// Define some basic server options
+const sessionCookeieName = "arcgis-rest-js-advanced-oauth-demo";
+const port = parseInt(process.env.PORT);
+
+// Create a new express and file store for our sessions
 const FileStore = SessionFileStore(session);
 
-/**
- * Create our oauth 2 options objects
- */
+// Create our oauth 2 options objects
 const oauthOptions = {
   clientId: process.env.CLIENT_ID,
   redirectUri: process.env.REDIRECT_URI
 };
 
-/**
- * Determine how long we want our session to last we know refresh tokens last 2 weeks so we subtract an hour to provide some padding.
- */
+// Determine how long we want our session to last we know refresh tokens last 2 weeks so we subtract an hour to provide some padding.
 const sessionTTL = 60 * 60 * 24 * 7 * 2 - 60 * 60 * 1; // 2 weeks - 1 hours in seconds;
 
-/**
- * Setup the session middleware
- */
+// Create the express app
+const app = express();
+app.set("etag", false); // disable caching for demo
+
+// Serve files from the static directory
+app.use(
+  express.static("static", {
+    etag: false
+  })
+);
+
+// Setup the session middleware
 app.use(
   session({
-    name: "arcgis-rest-js-advanced-oauth-demo", // the name of the cookie to store the session id.
-    secret: process.env.SESSION_SECRET, // secret used to sign the session cookie
-    resave: false,
+    name: sessionCookeieName, // The name of the cookie to store the session id.
+    secret: process.env.SESSION_SECRET, // Secret used to sign the session cookie
+    resave: true,
     saveUninitialized: false,
     cookie: {
-      maxAge: sessionTTL * 1000 // set the max age on the cookie to match our session duration
+      maxAge: sessionTTL * 1000 // Set the max age on the cookie to match our session duration
     },
 
-    // store session data in a secure, encrypted file, sessions will be loaded from these files and decrypted
-    // at the end of every request the state of `request.session` will be saved back to disk.
+    // Store session data in a secure, encrypted file, sessions will be loaded from these files and decrypted at the end of every request the state of `request.session` will be saved back to disk.
     store: new FileStore({
-      ttl: sessionTTL, // session duration
-      secret: process.env.ENCRYPTION_KEY, // secret used to encrypt and decrypt sessions on disk
+      ttl: sessionTTL, // Session duration
+      secret: process.env.ENCRYPTION_KEY, // Secret used to encrypt and decrypt sessions on disk
 
-      // define how to encode our session to text for storing in the file. We can use the `serialize()` method on the session for this.
+      // Define how to encode our session to text for storing in the file. We can use the `serialize()` method on the session for this.
       encoder: (sessionObj) => {
         sessionObj.arcgis = sessionObj.arcgis.serialize();
         return JSON.stringify(sessionObj);
       },
 
-      // define how to turn the text from the session data back into an object. We can use the `ArcGISIdentityManager.deserialize()` method for this.
+      // Define how to turn the text from the session data back into an object. We can use the `ArcGISIdentityManager.deserialize()` method for this.
       decoder: (sessionContents) => {
         const sessionObj = JSON.parse(sessionContents);
         if (sessionObj.arcgis) {
@@ -65,15 +73,15 @@ app.use(
   })
 );
 
-// Render a link to the authorization page on the homepage
-app.get("/", (request, response) => {
-  response.send(`<a href="/authorize">Sign in with ArcGIS</a>`);
+// Send index.html when the user lands on the homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("pages/index.html"));
 });
 
-// When a user visits the authorization page start the oauth 2 process. The `ArcGISIdentityManager.authorize()` method will redirect the user to the authorization page.
+// When a user visits the authorization page start the oAuth 2.0 process. The `ArcGISIdentityManager.authorize()` method will redirect the user to the authorization page.
 app.get("/authorize", function (request, response) {
-  // set a custom state value, this could be used to identify the user who started the request, here we just use a random string but this could be
-  // something like a user identifier.
+  // set a custom state value, this could be used to identify the user who started the request, here we just use a random
+  // string but this could be something like a user identifier.
   oauthOptions.state = randomBytes(20).toString("hex");
 
   console.log("Starting OAuth Request ID:", oauthOptions.state);
@@ -82,18 +90,18 @@ app.get("/authorize", function (request, response) {
   ArcGISIdentityManager.authorize(oauthOptions, response);
 });
 
-// the after authorizing the user is redirected to /authenticate and we can finish the Oauth 2.0 process.
+// After authorizing the user is redirected to /authenticate and we can finish the Oauth 2.0 process.
 app.get("/authenticate", async function (request, response) {
   console.log("Authorization complete for", request.query.state);
-  console.log(request.query);
-  // exchange the auth code for a an instance of `ArcGISIdentityManager` save that to the session.
+
+  // Exchange the auth code for a an instance of `ArcGISIdentityManager` save that to the session.
   request.session.arcgis =
     await ArcGISIdentityManager.exchangeAuthorizationCode(
       oauthOptions,
       request.query.code
     );
-  console.log(request.session.arcgis);
-  // once we have the session set redirect the user to the /app route so they can use the app.
+
+  // Once we have the session set redirect the user to the /app route so they can use the app.
   response.redirect("/app");
 });
 
@@ -105,19 +113,20 @@ app.get("/refresh", function (request, response) {
     return;
   }
 
-  // refresh the session
+  // Refresh the session by getting a new access token with our refresh token
   request.session.arcgis
     .refreshCredentials()
     .then((newSession) => {
+      // save the new session
       request.session.userSession = newSession;
 
-      // convert the session to an object and remove the refresh token
+      // Convert the session to an object and remove the refresh token
       const serializedSession = newSession.toJSON();
       delete serializedSession.refreshToken;
       delete serializedSession.refreshTokenExpires;
 
-      // respond with the new session data
-      request.json(serializedSession);
+      // Respond with the new session data
+      response.json(serializedSession);
     })
     .catch((error) => {
       response.json({
@@ -126,43 +135,54 @@ app.get("/refresh", function (request, response) {
     });
 });
 
-// This handles the application route
-app.get("/app", (request, response) => {
-  // if there is no session available redirect the user back to the home page
+// Sign the user out of our app by destroying the session the cookie and invalidating the session with ArcGIS
+app.get("/sign-out", (request, response) => {
   if (!request.session.arcgis) {
     response.redirect("/");
     return;
   }
-  console.log(request.session.arcgis);
-  // next exchange the current refresh token for a new refresh token, this extends the session for another 2 weeks
-  // we also read the template for the app.
-  Promise.all([
-    readFile("app.html", { encoding: "utf-8" }),
-    request.session.arcgis.exchangeRefreshToken()
-  ]).then(([templateContents, freshSession]) => {
-    // update our session object with the new session
-    request.session.arcgis = freshSession;
 
-    // prepare to send the session to the client
-    const serializedSession = freshSession.toJSON();
+  request.session.arcgis.signOut().then(() => {
+    request.session.destroy(() => {
+      response.clearCookie("arcgis-rest-js-advanced-oauth-demo");
+      response.redirect("/");
+    });
+  });
+});
+
+// This handles the application route
+app.get("/app", (request, response) => {
+  // If there is no session available redirect the user back to the home page
+  if (!request.session.arcgis) {
+    response.redirect("/");
+    return;
+  }
+
+  // Next exchange the current refresh token for a new refresh token, this extends the session for another 2 weeks
+  Promise.all([
+    readFile("pages/app.html", { encoding: "utf-8" }),
+    request.session.arcgis.exchangeRefreshToken()
+  ]).then(([templateContents, session]) => {
+    // Update our session object with the new session
+    request.session.arcgis = session;
+
+    // Prepare to send the session to the client
+    const serializedSession = session.toJSON();
     delete serializedSession.refreshToken;
     delete serializedSession.refreshTokenExpires;
 
-    // insert the session into the HTML
-    templateContents = templateContents.replace(
-      "<body>",
-      `<body><pre>${JSON.stringify(serializedSession, null, 2)}</pre>`
-    );
+    // Insert the session info into the HTML
     templateContents = templateContents.replace(
       "SESSION_JSON",
       JSON.stringify(serializedSession)
     );
 
-    // send the HTML
+    // Send the HTML
     response.send(templateContents);
   });
 });
 
-app.listen(3000, function () {
-  console.log("visit http://localhost:3000/authorize to test the application!");
+// Start the server
+app.listen(port, function () {
+  console.log(`visit http://localhost:${port} to test the application!`);
 });
