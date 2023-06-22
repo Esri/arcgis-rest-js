141fa6e8478eae5b064825b6d666bed4e3079494
Patrick Arlt <patrick.arlt@gmail.com>
2023-06-22 10:43:12 -0700

    fix(arcgis-rest-places): update pagination values for latest API

--- packages/arcgis-rest-places/src/utils.ts
+++ packages/arcgis-rest-places/src/utils.ts
@@ -2,7 +2,7 @@ export const baseUrl =
   "https://places-api.arcgis.com/arcgis/rest/services/places-service/v1";
 
 export function hasNextPage(response: any) {
-  return !!response?.links?.next;
+  return !!response?.links?.next || !!response?.pagination?.nextUrl;
 }
 
 export function getNextPageParams(currentOffset = 0, currentPageSize = 10) {
