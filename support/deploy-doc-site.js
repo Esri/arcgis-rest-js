1153e68ce698d7dbc7ab3e9b974d7a617079a804
Dave Bouwman <dave@davebouwman.com>
2024-03-26 14:18:08 -0600

    fix: update doc path

--- support/deploy-doc-site.js
+++ support/deploy-doc-site.js
@@ -1,12 +1,12 @@
 const ghpages = require("gh-pages");
 
 ghpages.publish(
-  "docs/build",
+  "docs/build/arcgis-rest-js",
   {
     branch: "gh-pages",
-    repo: "https://github.com/Esri/arcgis-rest-js.git"
+    repo: "https://github.com/Esri/arcgis-rest-js.git",
   },
-  function(err) {
+  function (err) {
     if (err) {
       console.log("uh oh", err);
     } else {
