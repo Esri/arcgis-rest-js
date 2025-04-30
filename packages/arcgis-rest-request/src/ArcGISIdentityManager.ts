082b1c67cfd6da8e90e29526d0bb0e03c1d9cf5b
Dave Bouwman <dave@davebouwman.com>
2025-04-30 10:07:06 -0600

    fix: postMessage must send a Credential that is usable by JSSDK (#1223)

--- packages/arcgis-rest-request/src/ArcGISIdentityManager.ts
+++ packages/arcgis-rest-request/src/ArcGISIdentityManager.ts
@@ -1317,7 +1317,7 @@ export class ArcGISIdentityManager
       if (isValidOrigin && isValidType) {
         let msg = {};
         if (isTokenValid) {
-          const credential = this.toJSON();
+          const credential = this.toCredential();
           msg = {
             type: "arcgis:auth:credential",
             credential
