e8493f0d427c0fe69e289425ff7ef16abed2c178
Dave Bouwman <dave@davebouwman.com>
2025-05-12 14:58:03 -0600

    Update index.ts
    update date in license, just to force release to run

--- packages/arcgis-rest-request/src/index.ts
+++ packages/arcgis-rest-request/src/index.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2019 Environmental Systems Research Institute, Inc.
+/* Copyright (c) 2018-2025 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
 export * from "./request.js";
