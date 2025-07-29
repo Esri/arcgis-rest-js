7c1ec7f35da90864b4d27aa1c3e6f93f53e0a41f
Patrick Arlt <parlt@esri.com>
2025-07-29 08:05:14 -0700

    feat(arcgis-rest-basemap-sessions): remove static basemap tiles

--- packages/arcgis-rest-basemap-sessions/src/index.ts
+++ packages/arcgis-rest-basemap-sessions/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./BaseSession.js";
-export * from "./StaticBasemapTilesSession.js";
 export * from "./BasemapStyleSession.js";
 export * from "./types/StyleFamily.js";
 export * from "./utils/defaults.js";
--- packages/arcgis-rest-basemap-sessions/src/utils/defaults.ts
+++ packages/arcgis-rest-basemap-sessions/src/utils/defaults.ts
@@ -1,9 +1,6 @@
 export const DEFAULT_START_BASEMAP_STYLE_SESSION_URL =
   "https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/sessions/start";
 
-export const DEFAULT_START_STATIC_BASEMAP_SESSION_URL =
-  "https://static-map-tiles-api.arcgis.com/arcgis/rest/services/static-basemap-tiles-service/v1/sessions/start";
-
 export const DEFAULT_SAFETY_MARGIN = 1000 * 60 * 5; // Default to 5 minutes
 
 export const DEFAULT_CHECK_EXPIRATION_INTERVAL = 1000 * 10; // Default to 1 minute
