77add72cbc0600533f4a4ff09b676394e1da8e51
Patrick Arlt <patrick.arlt@gmail.com>
2025-07-30 12:16:16 -0700

    Apply suggestion from @gavinr-maps
    Co-authored-by: Gavin Rehkemper <137905994+gavinr-maps@users.noreply.github.com>

--- packages/arcgis-rest-basemap-sessions/src/utils/writable.ts
+++ packages/arcgis-rest-basemap-sessions/src/utils/writable.ts
@@ -1,7 +1,7 @@
 /**
  * Utility type to convert all properties of an object type to writable.
  *
- * If needed a more complex version of is avalable at: https://github.com/sindresorhus/type-fest/blob/main/source/writable.d.ts
+ * If needed a more complex version of is available at: https://github.com/sindresorhus/type-fest/blob/main/source/writable.d.ts
  */
 export type Writable<T> = {
   -readonly [K in keyof T]: T[K];
