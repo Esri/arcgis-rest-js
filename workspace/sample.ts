bacb6ee9ba93ca7e4b3f57e57d04ef9ff5304011
KarshitShah <kar11185@esri.com>
2025-09-17 13:35:42 -0400

    feat(arcgis-rest-portal): update sample file

--- workspace/sample.ts
+++ workspace/sample.ts
@@ -15,7 +15,7 @@
 import { searchItems } from "../packages/arcgis-rest-portal/dist/esm/index.js";
 
 // If you are using node < 14.8, you need to wrap the await in an async IFFE
-// (async function () {
+//(async function () {
 const response = await searchItems("water");
 console.log(`Public item search for "water": ${response.total} total results`);
 response.results.map((itm) => {
