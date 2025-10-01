a2f039a3cd82f14bc82d2d1b30e0b84de668a6c7
Dixon Yant <dyant@esri.com>
2025-10-01 16:51:21 -0700

    chore: migrate and update tests to Vitest and remove redundant expect

--- packages/arcgis-rest-portal/test/users/invitation.test.ts
+++ packages/arcgis-rest-portal/test/users/invitation.test.ts
@@ -1,20 +1,19 @@
 /* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
+import { describe, test, expect, afterEach } from "vitest";
 import fetchMock from "fetch-mock";
+import { ArcGISIdentityManager, encodeParam } from "@esri/arcgis-rest-request";
 import {
   getUserInvitations,
   getUserInvitation,
   acceptInvitation,
   declineInvitation
 } from "../../src/users/invitation.js";
-
 import {
   UserInvitationsResponse,
   UserInvitationResponse
 } from "../mocks/users/invitation.js";
-
-import { ArcGISIdentityManager, encodeParam } from "@esri/arcgis-rest-request";
 
 const TOMORROW = (function () {
   const now = new Date();
@@ -35,93 +34,77 @@ describe("invitations", () => {
   });
 
   describe("getUserInvitations", () => {
-    it("should make an authenticated request for user invitations", (done) => {
+    test("should make an authenticated request for user invitations", async () => {
       fetchMock.once("*", UserInvitationsResponse);
 
-      getUserInvitations({ authentication: session })
-        .then((response) => {
-          expect(fetchMock.called()).toEqual(true);
-          const [url, options] = fetchMock.lastCall("*");
-          expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations?f=json&token=fake-token"
-          );
-          expect(options.method).toBe("GET");
-          expect(response.userInvitations.length).toEqual(1);
-          done();
-        })
-        .catch((e) => {
-          fail(e);
-        });
+      const response = await getUserInvitations({ authentication: session });
+      expect(fetchMock.called()).toEqual(true);
+      const [url, options] = fetchMock.lastCall("*");
+      expect(url).toEqual(
+        "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations?f=json&token=fake-token"
+      );
+      expect(options.method).toBe("GET");
+      expect(response.userInvitations.length).toEqual(1);
     });
   });
 
   describe("getUserInvitation", () => {
-    it("should make an authenticated request for a user invitation", (done) => {
+    test("should make an authenticated request for a user invitation", async () => {
       fetchMock.once("*", UserInvitationResponse);
 
-      getUserInvitation({ invitationId: "3ef", authentication: session })
-        .then((response) => {
-          expect(fetchMock.called()).toEqual(true);
-          const [url, options] = fetchMock.lastCall("*");
-          expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations/3ef?f=json&token=fake-token"
-          );
-          expect(options.method).toBe("GET");
-          expect(response.id).toEqual("G45ad52e7560e470598815499003c13f6");
-          expect(response.id).toEqual("G45ad52e7560e470598815499003c13f6");
-          done();
-        })
-        .catch((e) => {
-          fail(e);
-        });
+      const response = await getUserInvitation({
+        invitationId: "3ef",
+        authentication: session
+      });
+      expect(fetchMock.called()).toEqual(true);
+      const [url, options] = fetchMock.lastCall("*");
+      expect(url).toEqual(
+        "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations/3ef?f=json&token=fake-token"
+      );
+      expect(options.method).toBe("GET");
+      expect(response.id).toEqual("G45ad52e7560e470598815499003c13f6");
     });
   });
 
   describe("acceptInvitation", () => {
-    it("should accept an invitation", (done) => {
+    test("should accept an invitation", async () => {
       fetchMock.once("*", { success: true });
 
-      acceptInvitation({ invitationId: "3ef", authentication: session })
-        .then((response) => {
-          expect(fetchMock.called()).toEqual(true);
-          const [url, options] = fetchMock.lastCall("*");
-          expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations/3ef/accept"
-          );
-          expect(options.method).toBe("POST");
-          expect(options.body).toContain(encodeParam("f", "json"));
-          expect(options.body).toContain(encodeParam("token", "fake-token"));
-          expect(response.success).toEqual(true);
-          // expect(response.notificationId).toBe("3ef");
-          done();
-        })
-        .catch((e) => {
-          fail(e);
-        });
+      const response = await acceptInvitation({
+        invitationId: "3ef",
+        authentication: session
+      });
+      expect(fetchMock.called()).toEqual(true);
+      const [url, options] = fetchMock.lastCall("*");
+      expect(url).toEqual(
+        "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations/3ef/accept"
+      );
+      expect(options.method).toBe("POST");
+      expect(options.body).toContain(encodeParam("f", "json"));
+      expect(options.body).toContain(encodeParam("token", "fake-token"));
+      expect(response.success).toEqual(true);
+      // expect(response.notificationId).toBe("3ef");
     });
   });
 
   describe("declineInvitation", () => {
-    it("should decline an invitation", (done) => {
+    test("should decline an invitation", async () => {
       fetchMock.once("*", { success: true });
 
-      declineInvitation({ invitationId: "3ef", authentication: session })
-        .then((response) => {
-          expect(fetchMock.called()).toEqual(true);
-          const [url, options] = fetchMock.lastCall("*");
-          expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations/3ef/decline"
-          );
-          expect(options.method).toBe("POST");
-          expect(options.body).toContain(encodeParam("f", "json"));
-          expect(options.body).toContain(encodeParam("token", "fake-token"));
-          expect(response.success).toEqual(true);
-          // expect(response.notificationId).toBe("3ef");
-          done();
-        })
-        .catch((e) => {
-          fail(e);
-        });
+      const response = await declineInvitation({
+        invitationId: "3ef",
+        authentication: session
+      });
+      expect(fetchMock.called()).toEqual(true);
+      const [url, options] = fetchMock.lastCall("*");
+      expect(url).toEqual(
+        "https://myorg.maps.arcgis.com/sharing/rest/community/users/c%40sey/invitations/3ef/decline"
+      );
+      expect(options.method).toBe("POST");
+      expect(options.body).toContain(encodeParam("f", "json"));
+      expect(options.body).toContain(encodeParam("token", "fake-token"));
+      expect(response.success).toEqual(true);
+      // expect(response.notificationId).toBe("3ef");
     });
   });
 });
