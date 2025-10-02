e9992d6327be415c37d14a22075b02767c8004cd
Dixon Yant <dyant@esri.com>
2025-10-02 15:51:27 -0700

    chore: migrate and update tests to Vitest search

--- packages/arcgis-rest-portal/test/users/search.test.ts
+++ packages/arcgis-rest-portal/test/users/search.test.ts
@@ -2,11 +2,12 @@
  * Apache-2.0 */
 
 import fetchMock from "fetch-mock";
+import { UserSearchResponse } from "../mocks/users/user-search.js";
+import { describe, test, expect, afterEach } from "vitest";
 import {
   searchUsers,
   searchCommunityUsers
 } from "../../src/users/search-users.js";
-import { UserSearchResponse } from "../mocks/users/user-search.js";
 
 describe("users", () => {
   const MOCK_AUTH = {
@@ -19,53 +20,42 @@ describe("users", () => {
   afterEach(() => {
     fetchMock.restore();
   });
+
   describe("searchUsers", () => {
-    it("should make a simple, authenticated user search request", (done) => {
+    test("should make a simple, authenticated user search request", async () => {
       fetchMock.once("*", UserSearchResponse);
 
-      searchUsers({
+      const response = await searchUsers({
         q: "role:org_user OR role:org_publisher",
         num: 100,
         authentication: MOCK_AUTH
-      })
-        .then((response) => {
-          expect(fetchMock.called()).toEqual(true);
-          const [url, options] = fetchMock.lastCall("*");
-          expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/portals/self/users/search?f=json&q=role%3Aorg_user%20OR%20role%3Aorg_publisher&num=100&token=fake-token"
-          );
-          expect(options.method).toBe("GET");
-          expect(response).toEqual(UserSearchResponse);
-          done();
-        })
-        .catch((e) => {
-          fail(e);
-        });
+      });
+      expect(fetchMock.called()).toEqual(true);
+      const [url, options] = fetchMock.lastCall("*");
+      expect(url).toEqual(
+        "https://myorg.maps.arcgis.com/sharing/rest/portals/self/users/search?f=json&q=role%3Aorg_user%20OR%20role%3Aorg_publisher&num=100&token=fake-token"
+      );
+      expect(options.method).toBe("GET");
+      expect(response).toEqual(UserSearchResponse);
     });
   });
 
   describe("searchCommunityUsers", () => {
-    it("should make a simple, authenticated user search request", (done) => {
+    test("should make a simple, authenticated user search request", async () => {
       fetchMock.once("*", UserSearchResponse);
 
-      searchCommunityUsers({
+      const response = await searchCommunityUsers({
         q: "role:org_user OR role:org_publisher",
         num: 100,
         authentication: MOCK_AUTH
-      })
-        .then((response) => {
-          expect(fetchMock.called()).toEqual(true);
-          const [url, options] = fetchMock.lastCall("*");
-          expect(url).toEqual(
-            "https://myorg.maps.arcgis.com/sharing/rest/community/users?f=json&q=role%3Aorg_user%20OR%20role%3Aorg_publisher&num=100&token=fake-token"
-          );
-          expect(options.method).toBe("GET");
-          expect(response).toEqual(UserSearchResponse);
-          done();
-        })
-        .catch((e) => {
-          fail(e);
-        });
+      });
+      expect(fetchMock.called()).toEqual(true);
+      const [url, options] = fetchMock.lastCall("*");
+      expect(url).toEqual(
+        "https://myorg.maps.arcgis.com/sharing/rest/community/users?f=json&q=role%3Aorg_user%20OR%20role%3Aorg_publisher&num=100&token=fake-token"
+      );
+      expect(options.method).toBe("GET");
+      expect(response).toEqual(UserSearchResponse);
     });
   });
 });
