4f1ee1175bc387436a3151bcdaf9865177e10202
Dixon Yant <dyant@esri.com>
2025-10-03 11:15:17 -0700

    chore: migrate tests to Vitest for deprecated getUserUrl

--- packages/arcgis-rest-portal/test/users/get-user-url.test.ts
+++ packages/arcgis-rest-portal/test/users/get-user-url.test.ts
@@ -2,38 +2,35 @@
  * Apache-2.0 */
 
 import { ArcGISIdentityManager } from "@esri/arcgis-rest-request";
-
 import { getUserUrl } from "../../src/users/get-user-url.js";
+import { describe, test, expect } from "vitest";
 
 describe("getUserUrl", () => {
-  it("should encode special characters", () => {
+  test("should encode special characters", () => {
     const session = new ArcGISIdentityManager({
       username: "c@sey"
     });
-
-    expect(getUserUrl(session)).toEqual(
+    expect(getUserUrl(session)).toBe(
       "https://www.arcgis.com/sharing/rest/community/users/c%40sey"
     );
   });
 
-  it("should recognize an explicit ArcGIS Online organization", () => {
+  test("should recognize an explicit ArcGIS Online organization", () => {
     const session = new ArcGISIdentityManager({
       username: "c@sey",
       portal: "https://custom.maps.arcgis.com/sharing/rest"
     });
-
-    expect(getUserUrl(session)).toEqual(
+    expect(getUserUrl(session)).toBe(
       "https://custom.maps.arcgis.com/sharing/rest/community/users/c%40sey"
     );
   });
 
-  it("should recognize ArcGIS Enterprise", () => {
+  test("should recognize ArcGIS Enterprise", () => {
     const session = new ArcGISIdentityManager({
       username: "c@sey",
       portal: "https://gis.city.gov/sharing/rest"
     });
-
-    expect(getUserUrl(session)).toEqual(
+    expect(getUserUrl(session)).toBe(
       "https://gis.city.gov/sharing/rest/community/users/c%40sey"
     );
   });
