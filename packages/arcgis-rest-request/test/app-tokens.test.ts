1bfa82f9370cb212857cee1d2594158cb784e82e
Dixon Yant <dyant@esri.com>
2025-10-16 14:56:11 -0700

    chore: migrating request tests to Vitest

--- packages/arcgis-rest-request/test/app-tokens.test.ts
+++ packages/arcgis-rest-request/test/app-tokens.test.ts
@@ -3,84 +3,78 @@
 
 import fetchMock from "fetch-mock";
 import { exchangeToken, platformSelf } from "../src/app-tokens.js";
+import { describe, test, expect } from "vitest";
 
 describe("app-token functions: ", () => {
   describe("exchangeToken:", () => {
-    it("makes a request to /oauth2/exchangeToken passing params", () => {
+    test("makes a request to /oauth2/exchangeToken passing params", async () => {
       const EXCHANGE_TOKEN_URL =
         "https://www.arcgis.com/sharing/rest/oauth2/exchangeToken";
       fetchMock.postOnce(EXCHANGE_TOKEN_URL, {
         token: "APP-TOKEN"
       });
-      return exchangeToken("FAKE-TOKEN", "CLIENT-ID-ABC123")
-        .then((response) => {
-          const [url, options] = fetchMock.lastCall(EXCHANGE_TOKEN_URL);
-          expect(url).toEqual(EXCHANGE_TOKEN_URL);
-          expect(options.body).toContain("f=json");
-          expect(options.body).toContain("token=FAKE-TOKEN");
-          expect(options.body).toContain("client_id=CLIENT-ID-ABC123");
-          expect(response).toEqual("APP-TOKEN");
-        })
-        .catch((e) => fail(e));
+      const response = await exchangeToken("FAKE-TOKEN", "CLIENT-ID-ABC123");
+      const [url, options] = fetchMock.lastCall(EXCHANGE_TOKEN_URL);
+      expect(url).toEqual(EXCHANGE_TOKEN_URL);
+      expect(options.body).toContain("f=json");
+      expect(options.body).toContain("token=FAKE-TOKEN");
+      expect(options.body).toContain("client_id=CLIENT-ID-ABC123");
+      expect(response).toEqual("APP-TOKEN");
     });
-    it("takes a portalUrl", () => {
+    test("takes a portalUrl", async () => {
       const PORTAL_BASE_URL = "https://my-portal.com/instance/sharing/rest";
       const PORTAL_EXCHANGE_URL = `${PORTAL_BASE_URL}/oauth2/exchangeToken`;
       fetchMock.postOnce(PORTAL_EXCHANGE_URL, {
         valid: true,
         viewOnlyUserTypeApp: false
       });
-      return exchangeToken("FAKE-TOKEN", "CLIENT-ID-ABC123", PORTAL_BASE_URL)
-        .then((response) => {
-          const [url, options] = fetchMock.lastCall(PORTAL_EXCHANGE_URL);
-          expect(url).toEqual(PORTAL_EXCHANGE_URL);
-        })
-        .catch((e) => fail(e));
+      const response = await exchangeToken(
+        "FAKE-TOKEN",
+        "CLIENT-ID-ABC123",
+        PORTAL_BASE_URL
+      );
+      const [url, options] = fetchMock.lastCall(PORTAL_EXCHANGE_URL);
+      expect(url).toEqual(PORTAL_EXCHANGE_URL);
     });
   });
 
   describe("platformSelf:", () => {
-    it("makes a request to /oauth2/platformSelf passing params", () => {
+    test("makes a request to /oauth2/platformSelf passing params", async () => {
       const PLATFORM_SELF_URL =
         "https://www.arcgis.com/sharing/rest/oauth2/platformSelf?f=json";
       fetchMock.postOnce(PLATFORM_SELF_URL, {
         username: "jsmith",
         token: "APP-TOKEN"
       });
-      return platformSelf(
+      const response = await platformSelf(
         "CLIENT-ID-ABC123",
         "https://hub.arcgis.com/torii-provider-arcgis/redirect.html"
-      )
-        .then((response) => {
-          const [url, options] = fetchMock.lastCall(PLATFORM_SELF_URL);
-          expect(url).toEqual(PLATFORM_SELF_URL);
-          const headers = options.headers || ({} as any);
-          expect(headers["X-Esri-Auth-Redirect-Uri"]).toBe(
-            "https://hub.arcgis.com/torii-provider-arcgis/redirect.html"
-          );
-          expect(options.credentials).toBe(
-            "include",
-            "fetch should send cookie"
-          );
-          expect(headers["X-Esri-Auth-Client-Id"]).toBe("CLIENT-ID-ABC123");
-          expect(response.token).toEqual("APP-TOKEN");
-          expect(response.username).toEqual("jsmith");
-        })
-        .catch((e) => fail(e));
+      );
+      const [url, options] = fetchMock.lastCall(PLATFORM_SELF_URL);
+      expect(url).toEqual(PLATFORM_SELF_URL);
+      const headers = options.headers || ({} as any);
+      expect(headers["X-Esri-Auth-Redirect-Uri"]).toBe(
+        "https://hub.arcgis.com/torii-provider-arcgis/redirect.html"
+      );
+      expect(options.credentials).toBe("include"); // fetch should send cookie
+      expect(headers["X-Esri-Auth-Client-Id"]).toBe("CLIENT-ID-ABC123");
+      expect(response.token).toEqual("APP-TOKEN");
+      expect(response.username).toEqual("jsmith");
     });
-    it("takes a portalUrl", () => {
+    test("takes a portalUrl", async () => {
       const PORTAL_BASE_URL = "https://my-portal.com/instance/sharing/rest";
       const PORTAL_PLATFORM_SELF_URL = `${PORTAL_BASE_URL}/oauth2/platformSelf?f=json`;
       fetchMock.postOnce(PORTAL_PLATFORM_SELF_URL, {
         username: "jsmith",
         token: "APP-TOKEN"
       });
-      return platformSelf("FAKE-TOKEN", "CLIENT-ID-ABC123", PORTAL_BASE_URL)
-        .then((response) => {
-          const [url, options] = fetchMock.lastCall(PORTAL_PLATFORM_SELF_URL);
-          expect(url).toEqual(PORTAL_PLATFORM_SELF_URL);
-        })
-        .catch((e) => fail(e));
+      const response = await platformSelf(
+        "FAKE-TOKEN",
+        "CLIENT-ID-ABC123",
+        PORTAL_BASE_URL
+      );
+      const [url, options] = fetchMock.lastCall(PORTAL_PLATFORM_SELF_URL);
+      expect(url).toEqual(PORTAL_PLATFORM_SELF_URL);
     });
   });
 });
--- packages/arcgis-rest-request/test/validate-app-access.test.ts
+++ packages/arcgis-rest-request/test/validate-app-access.test.ts
@@ -3,40 +3,40 @@
 
 import fetchMock from "fetch-mock";
 import { validateAppAccess } from "../src/index.js";
+import { describe, test, expect } from "vitest";
 
 const VERIFYAPPACCESS_URL =
   "https://www.arcgis.com/sharing/rest/oauth2/validateAppAccess";
 
 describe("validateAppAccess: ", () => {
-  it("makes a request to /oauth2/validateAppAccess passing params", () => {
+  test("makes a request to /oauth2/validateAppAccess passing params", async () => {
     fetchMock.postOnce(VERIFYAPPACCESS_URL, {
       valid: true,
       viewOnlyUserTypeApp: false
     });
-    return validateAppAccess("FAKE-TOKEN", "abc123")
-      .then((response) => {
-        const [url, options] = fetchMock.lastCall(VERIFYAPPACCESS_URL);
-        expect(url).toEqual(VERIFYAPPACCESS_URL);
-        expect(options.body).toContain("f=json");
-        expect(options.body).toContain("token=FAKE-TOKEN");
-        expect(options.body).toContain("client_id=abc123");
-        expect(response.valid).toEqual(true);
-        expect(response.viewOnlyUserTypeApp).toBe(false);
-      })
-      .catch((e) => fail(e));
+    const response = await validateAppAccess("FAKE-TOKEN", "abc123");
+    const [url, options] = fetchMock.lastCall(VERIFYAPPACCESS_URL);
+    expect(url).toEqual(VERIFYAPPACCESS_URL);
+    expect(options.body).toContain("f=json");
+    expect(options.body).toContain("token=FAKE-TOKEN");
+    expect(options.body).toContain("client_id=abc123");
+    expect(response.valid).toEqual(true);
+    expect(response.viewOnlyUserTypeApp).toBe(false);
   });
-  it("takes a portalUrl", () => {
+
+  test("takes a portalUrl", async () => {
     const PORTAL_BASE_URL = "https://my-portal.com/instance/sharing/rest";
     const PORTAL_VERIFY_URL = `${PORTAL_BASE_URL}/oauth2/validateAppAccess`;
     fetchMock.postOnce(PORTAL_VERIFY_URL, {
       valid: true,
       viewOnlyUserTypeApp: false
     });
-    return validateAppAccess("FAKE-TOKEN", "abc123", PORTAL_BASE_URL)
-      .then((response) => {
-        const [url, options] = fetchMock.lastCall(PORTAL_VERIFY_URL);
-        expect(url).toEqual(PORTAL_VERIFY_URL);
-      })
-      .catch((e) => fail(e));
+    const response = await validateAppAccess(
+      "FAKE-TOKEN",
+      "abc123",
+      PORTAL_BASE_URL
+    );
+    const [url, options] = fetchMock.lastCall(PORTAL_VERIFY_URL);
+    expect(url).toEqual(PORTAL_VERIFY_URL);
   });
 });
