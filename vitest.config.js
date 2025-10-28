05ccd120b4723aab79dc039cd4ba9caf0c45b2c7
Dixon Yant <dyant@esri.com>
2025-10-28 10:20:03 -0700

    chore: wip install dependencies and configure vitest 4 browser testing

--- vitest.config.js
+++ vitest.config.js
@@ -1,39 +1,16 @@
 import { defineConfig } from "vite";
+import {
+  nodeProject,
+  browserHeadlessProject,
+  browserHeadedProject
+} from "./vitest.projects.ts";
 
 export default defineConfig({
   test: {
-    include: [
-      "packages/arcgis-rest-basemap-sessions/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-elevation/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-places/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-demographics/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-geocoding/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-routing/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-feature-service/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-developer-credentials/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-portal/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-      "packages/arcgis-rest-request/**/*.{test,spec}.?(c|m)[jt]s?(x)",
-    ],
-    coverage: {
-      enabled: true,
-      include: [
-        "packages/arcgis-rest-basemap-sessions/src/**/*.{ts,js}",
-        "packages/arcgis-rest-elevation/src/**/*.{ts,js}",
-        "packages/arcgis-rest-places/src/**/*.{ts,js}",
-        "packages/arcgis-rest-demographics/src/**/*.{ts,js}",
-        "packages/arcgis-rest-geocoding/src/**/*.{ts,js}",
-        "packages/arcgis-rest-routing/src/**/*.{ts,js}",
-        "packages/arcgis-rest-feature-service/src/**/*.{ts,js}",
-        "packages/arcgis-rest-developer-credentials/src/**/*.{ts,js}",
-        "packages/arcgis-rest-portal/src/**/*.{ts,js}",
-        "packages/arcgis-rest-request/src/**/*.{ts,js}"
-      ],
-      provider: "istanbul",
-      reporter: ["json", "html", "cobertura"],
-      reportsDirectory: "./coverage/vitest",
-      thresholds: {
-        100: true
-      }
-    }
+    projects: [
+      nodeProject, 
+      browserHeadlessProject,
+      browserHeadedProject
+    ]
   }
 });
