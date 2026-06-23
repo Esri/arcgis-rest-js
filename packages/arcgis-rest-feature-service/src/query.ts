db432b2460333d5c3c7e3fea588ace432e5d17a6
Dixon Yant <dyant@esri.com>
2026-06-23 14:51:42 -0700

    fix: restore params fallback in query pagination merge

--- packages/arcgis-rest-feature-service/src/query.ts
+++ packages/arcgis-rest-feature-service/src/query.ts
@@ -456,7 +456,7 @@ export async function queryAllFeatures(
       params: {
         where: "1=1",
         outFields: "*",
-        ...requestOptions.params,
+        ...(requestOptions.params || {}),
         resultOffset: offset,
         resultRecordCount: recordCountToUse
       }
