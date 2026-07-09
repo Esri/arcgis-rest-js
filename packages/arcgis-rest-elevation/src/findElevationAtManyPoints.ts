46d1fdc3937c5e8acc5d721ad9053bc8ef1d89a6
Dixon Yant <dyant@esri.com>
2026-07-09 14:36:20 -0700

    refactor (elevation): processOptions

--- packages/arcgis-rest-elevation/src/findElevationAtManyPoints.ts
+++ packages/arcgis-rest-elevation/src/findElevationAtManyPoints.ts
@@ -1,6 +1,6 @@
 import {
   request,
-  appendCustomParams,
+  processOptions,
   IRequestOptions
 } from "@esri/arcgis-rest-request";
 
@@ -8,7 +8,7 @@ import { operations } from "./openapi-types.js";
 import { baseUrl } from "./utils.js";
 
 // determine the list of allowed params we want to allow as options
-// this should match the array given to appendCustomParams below
+// this should match the array given to processOptions below
 type queryParams = Pick<
   operations["ElevationAtManyPointsPost"]["requestBody"]["content"]["application/json"],
   "coordinates" | "relativeTo"
@@ -72,15 +72,14 @@ export interface IFindElevationAtManyPointsOptions
 export function findElevationAtManyPoints(
   requestOptions: IFindElevationAtManyPointsOptions
 ): Promise<IFindElevationAtManyPointsResponse> {
-  const options: any = appendCustomParams<IFindElevationAtManyPointsOptions>(
-    requestOptions,
-    ["relativeTo"],
-    {
-      ...requestOptions
-    }
-  );
+  const { requestOptions: options } = processOptions(requestOptions, {
+    paramKeys: ["relativeTo"],
+    extractKeys: []
+  });
 
-  options.params.coordinates = JSON.stringify(requestOptions.coordinates);
+  if (options.params?.coordinates) {
+    options.params.coordinates = JSON.stringify(requestOptions.coordinates);
+  }
 
   return (
     request(`${baseUrl}/elevation/at-many-points`, {
